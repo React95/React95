9d0687821d385887676630dcd70c49aed4c97d52
Allysson dos Santos <aallysson0@gmail.com>
2018-03-30 14:13:05 -0300

    Configure babel and add a new story to storybook

---
+++ components/Button/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => (
+  <button>Hello button</button>
+);
+
+export default Button;
---
+++ stories/button.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from '../components/Button';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Button', module).add('default', () => <Button />);
