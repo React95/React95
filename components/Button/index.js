a5417c3488d8a6e4b0229a417399e105dc38091e
Allysson dos Santos <aallysson0@gmail.com>
2018-03-30 18:33:27 -0300

    Add simple buttom

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => (
-  <button>Hello button</button>
-);
+const Button = styled.button`
+  background-color: #BFBFBF;
+  padding: 3px 10px 5px;
+  border: 1px solid #000;
+  box-shadow: inset 1px 1px 1px #fff
+  border-bottom-width: 2px;
+  border-right-width: 2px;
+`;
 
 export default Button;
