8f0234b85d5378eed89c4ea53aa99033d7bac399
ggdaltoso <ggdaltoso@gmail.com>
2018-04-04 21:17:03 -0300

    Font and borders

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  @font-face {
+    font-family: 'Pixelade';
+    src: url('/fonts/Pixelade.woff2') format('woff2'),
+        url('/fonts/Pixelade.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  * {
+    font-family: 'Pixelade';
+  }
+`
 
 const Button = styled.button`
-  background-color: #BFBFBF;
-  padding: 3px 10px 5px;
-  border: 1px solid #000;
-  box-shadow: inset 1px 1px 1px #fff
-  border-bottom-width: 2px;
-  border-right-width: 2px;
+  background-color: #c3c7cb;
+  padding: 6px 20px 5px;
+  border: none;
+  font-size: 20px;
+
+  box-shadow:  inset 1px 1px 0px 1px #ffffff,
+               inset -1px -1px 0px 1px #868a8e;
+
+  &:disabled {
+    color: #868a8e;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: inset 1px 1px 0px 1px #ffffff,
+                inset -1px -1px 0px 1px #868a8e,
+                1px 1px 0 2px #000;
+  }
 `;
 
 export default Button;
--- stories/button.stories.js
+++ stories/button.stories.js
@@ -3,4 +3,4 @@ import Button from '../components/Button';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Button', module).add('default', () => <Button>OK</Button>);
+storiesOf('Button', module).add('default', () => <Button>Open</Button>);
