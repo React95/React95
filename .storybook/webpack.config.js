7f6c21e125f78065980e17f062434995a0d29d20
ggdaltoso <ggdaltoso@gmail.com>
2018-05-07 21:43:11 -0300

    add file-loader to webpack

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loaders: ['file-loader'],
+        include: path.resolve(__dirname, '../')
+      }
+    ]
+  }
+};
