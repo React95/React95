8a8147ac77fc70ec59c255b0868fbcc998aa31a5
ggdaltoso <ggdaltoso@gmail.com>
2018-05-20 20:54:09 -0300

    fix icon grid

--- stories/icons.stories.js
+++ stories/icons.stories.js
@@ -6,17 +6,21 @@ import icons from '../components/Icon/icons';
 import { storiesOf } from '@storybook/react';
 
 const IconList = styled.ul`
+  width: 50%;
   margin: 0;
   padding: 0;
 
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
+  grid-template-columns: repeat(6, 1fr);
 `;
 
 const IconBox = styled.li`
-  padding: 10px;
+  padding: 6px 0;
+
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 storiesOf('Icon', module).add('default', () => (
