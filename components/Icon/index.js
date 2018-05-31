b7cb6f0eed69036e98b7873d2e5877b63388eb6d
ggdaltoso <ggdaltoso@gmail.com>
2018-05-31 17:04:03 -0300

    JSX: Icon

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -1,26 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled, { injectGlobal } from 'styled-components';
-import icons from './icons.js';
-
-const I = styled.i`
-  display: block;
-  width: 30px;
-  height: 32px;
-
-  background-image: url('${({ name }) => icons[name] || 'none'}');
-  background-repeat: no-repeat;
-  background-position: center;
-`;
-
-const Icon = ({ name }) => <I name={name} />;
-
-Icon.propTypes = {
-  name: PropTypes.string,
-};
-
-Icon.defaultProps = {
-  name: null,
-};
+import Icon from './Icon';
 
 export default Icon;
