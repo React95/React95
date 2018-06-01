25acb6d1a31a9bd251f41a4ad6aa8d1337b9430a
ggdaltoso <ggdaltoso@gmail.com>
2018-06-01 18:21:38 -0300

    added export file

---
+++ index.js
@@ -0,0 +1,7 @@
+import Button from './components/Button';
+import Icon from './components/Icon';
+import Input from './components/Input';
+import Modal from './components/Modal';
+import TextArea from './components/TextArea';
+
+export { Button, Icon, Input, Modal, TextArea };
