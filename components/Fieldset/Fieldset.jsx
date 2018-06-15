e4169390cf668d4a94c19ffce7545c6340e8f5bc
ggdaltoso <ggdaltoso@gmail.com>
2018-06-14 22:40:57 -0300

    finish fieldset

--- components/Fieldset/Fieldset.jsx
+++ components/Fieldset/Fieldset.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Field = styled.fieldset`
+  border-top-style: groove;
+  border-top-width: 2px;
+  border-top-color: #e6e6e6;
+
+  border-left-style: groove;
+  border-left-width: 2px;
+  border-left-color: #e6e6e6;
+
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  border-bottom-color: #848284;
+
+  border-right-style: solid;
+  border-right-width: 1px;
+  border-right-color: #848284;
+
+  box-shadow: 1px 1px 0 0 #e6e6e6 /* inset 1px 1px 0 0 #e6e6e6 */;
 `;
 
-const Fieldset = props => (<Field {...props} />);
+const Fieldset = ({ legend, children, ...rest }) => (
+  <Field {...rest}>
+    {legend && <legend>{legend}</legend>}
+    {children}
+  </Field>
+);
 
+Fieldset.propTypes = {
+  legend: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
 
 export default Fieldset;
--- stories/fieldset.stories.js
+++ stories/fieldset.stories.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import Fieldset from '../components/Fieldset';
+import Checkbox from '../components/Checkbox';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Fieldset', module).add('default', () => <Fieldset />);
+const CheckboxList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+storiesOf('Fieldset', module).add('default', () => (
+  <Fieldset legend="Connection Settings" style={{ width: '250px' }}>
+    <CheckboxList>
+      <Checkbox checked={false}>Disable Remote Keyboard & Pointer</Checkbox>
+      <Checkbox checked={false}>Disable Local Keyboard & Pointer</Checkbox>
+      <Checkbox checked={true}>Remove Desktop Wallpaper</Checkbox>
+    </CheckboxList>
+  </Fieldset>
+));
