7609bf8ec51a35584b2108a4419b45a69dfec0aa
ggdaltoso <ggdaltoso@gmail.com>
2018-06-14 21:45:23 -0300

    Fieldset story

---
+++ stories/fieldset.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Fieldset from '../components/Fieldset';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Fieldset', module).add('default', () => <Fieldset />);
