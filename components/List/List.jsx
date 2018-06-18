f1a1e960c13e760df744ec2892539685dbdbae91
ggdaltoso <ggdaltoso@gmail.com>
2018-06-17 21:25:36 -0300

    List component scaffold

---
+++ components/List/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Field = styled.ul``;
+
+class List extends React.Component {
+  render() {
+    return <Field />;
+  }
+}
+
+List.propTypes = {};
+
+List.defaultProps = {};
+
+export default List;
