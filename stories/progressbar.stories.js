24ac67e90e274b61346732380328ce5df68d1f53
ggdaltoso <ggdaltoso@gmail.com>
2018-07-01 17:55:24 -0300

    ProgressBar storie updated

--- stories/progressbar.stories.js
+++ stories/progressbar.stories.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { WithClippy } from '../.storybook/src/clippy-addon/clippy-addon';
+
 import ProgressBar from '../components/ProgressBar';
 
-import { storiesOf } from '@storybook/react';
-
-storiesOf('ProgressBar', module).add('default', () => <ProgressBar width={200} percent={49} />);
+storiesOf('ProgressBar', module).add('default', () => (
+  <WithClippy
+    component="ProgressBar"
+    code="<ProgressBar width={200} percent={49} />"
+  >
+    <ProgressBar width={200} percent={49} />
+  </WithClippy>
+));
