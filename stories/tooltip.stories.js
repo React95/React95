676e08482cfeec772100c8b806a3ca7e3f76808a
ggdaltoso <ggdaltoso@gmail.com>
2018-07-01 17:59:31 -0300

    Tooltip storie updated

--- stories/tooltip.stories.js
+++ stories/tooltip.stories.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { WithClippy } from '../.storybook/src/clippy-addon/clippy-addon';
+
 import Tooltip from '../components/Tooltip';
 
-import { storiesOf } from '@storybook/react';
+const code = `<Tooltip text={actualDate} delay={1000}>
+  <span>Hover me</span>
+</Tooltip>
+`;
 
 storiesOf('Tooltip', module).add('default', () => (
-  <div>
+  <WithClippy component="Tooltip" code={code}>
     <br />
     <br />
     <br />
     <Tooltip>
       <span>Hover me</span>
     </Tooltip>
-  </div>
+  </WithClippy>
 ));
