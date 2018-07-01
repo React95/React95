44d1c0d11592b269a56dc5ab4a83b8d3bfaff9aa
ggdaltoso <ggdaltoso@gmail.com>
2018-07-01 18:00:35 -0300

    Tooltip storie uptated

--- stories/tooltip.stories.js
+++ stories/tooltip.stories.js
@@ -4,7 +4,32 @@ import { WithClippy } from '../.storybook/src/clippy-addon/clippy-addon';
 
 import Tooltip from '../components/Tooltip';
 
-const code = `<Tooltip text={actualDate} delay={1000}>
+function formatDate(date) {
+  var monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return (
+    day.toString().padStart(2, '0') + ' ' + monthNames[monthIndex] + ' ' + year
+  );
+}
+
+const code = `<Tooltip text={${formatDate(new Date())}} delay={1000}>
   <span>Hover me</span>
 </Tooltip>
 `;
