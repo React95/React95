e83f1d38cec4189039bbc8070b7d3a7c35ba12c5
ggdaltoso <ggdaltoso@gmail.com>
2018-07-01 16:50:59 -0300

    Dropdown storie updated

--- stories/dropdown.stories.js
+++ stories/dropdown.stories.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import Dropdown from '../components/Dropdown';
-
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Dropdown', module).add('default', () => <Dropdown />);
+import { WithClippy } from '../.storybook/src/clippy-addon/clippy-addon';
+import Dropdown from '../components/Dropdown';
+
+const code = `<Dropdown
+  options={[
+    '',
+    'C:\\Documents and Settings',
+    'C:\\Documents and Settings\\Documents',
+    'iexplorer.exe',
+  ]}
+/>
+`;
+
+storiesOf('Dropdown', module).add('default', () => (
+  <WithClippy component="Dropdown" code={code}>
+    <Dropdown />
+  </WithClippy>
+));
