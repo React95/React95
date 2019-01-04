741b719aaf70a217494b09d62e22359a2157daac
Allysson Santos <allysson.santos@catho.com>
2019-01-04 15:51:26 -0200

    organize css

--- components/Range/Range.jsx
+++ components/Range/Range.jsx
@@ -11,8 +11,8 @@ const trackStyle = css`
   cursor: pointer;
 
   border-top: 1px solid #ccc;
-  border-bottom: 1px solid #eee;
   border-right: 2px solid #fff;
+  border-bottom: 1px solid #eee;
   border-left: 2px solid #fff;
   
   box-shadow: 0px 1px 0px 0px #fff;
@@ -29,11 +29,14 @@ const thumbStyle = css`
   margin-top: -8px;
 
   border-top: 1px solid #fff;
-  border-left: 1px solid #fff;
-  border-bottom: 1px solid #000;
   border-right: 1px solid #000;
+  border-bottom: 1px solid #000;
+  border-left: 1px solid #fff;
 
-  box-shadow: inset 0px -1px 0px #aaa, inset -1px 0px 0px #aaa, inset 0px 1px 0px #dedede, inset 1px 0px 0px #dedede;
+  box-shadow: inset 0px -1px 0px #aaa,
+    inset -1px 0px 0px #aaa,
+    inset 0px 1px 0px #dedede,
+    inset 1px 0px 0px #dedede;
 
   -webkit-appearance: none;
 `;
@@ -44,56 +47,24 @@ const Range = styled.input.attrs({
   -webkit-appearance: none;
   width: 100%;
 
-  &:hover, &:focus, &:active {
+  &:hover,
+  &:focus,
+  &:active {
     outline: none;
   }
 
-  &::-webkit-slider-runnable-track {
+  &::-webkit-slider-runnable-track,
+  &::-moz-range-track {
     ${trackStyle}
   }
 
-  &::-webkit-slider-thumb {
+  &::-webkit-slider-thumb,
+  &::-moz-range-thumb {
     ${thumbStyle}
   }
 
   &:focus::-webkit-slider-runnable-track {
     background: #000;
-  }
-
-  &::-moz-range-track {
-    width: 100%;
-    height: 4px;
-
-    background: #000;
-
-    cursor: pointer;
-
-    border-top: 1px solid #ccc;
-    border-bottom: 1px solid #eee;
-    border-right: 2px solid #fff;
-    border-left: 2px solid #fff;
-
-    box-shadow: 0px 1px 0px 0px #fff;
-  }
-
-  &::-moz-range-thumb {
-    width: 12px;
-    height: 20px;
-
-    background: #c3c7cb;
-
-    cursor: pointer;
-
-    margin-top: -7px;
-
-    border-top: 1px solid #fff;
-    border-left: 1px solid #fff;
-    border-bottom: 1px solid #000;
-    border-right: 1px solid #000;
-
-    box-shadow: inset 0px -1px 0px #aaa, inset -1px 0px 0px #aaa, inset 0px 1px 0px #dedede, inset 1px 0px 0px #dedede;
-  
-    -webkit-appearance: none;
   }
 `;
 
