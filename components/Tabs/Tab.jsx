0a73fa993896355cf554483413efe2da04fe2ce9
ggdaltoso <ggdaltoso@gmail.com>
2019-01-23 20:32:06 -0200

    Tabs and Tab skeleton

---
+++ components/Tabs/Tab.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class Tab extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return '';
+  }
+}
+
+Tab.propTypes = {};
+
+Tab.defaultProps = {};
+
+export default Tab;
---
+++ components/Tabs/Tabs.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class Tabs extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return '';
+  }
+}
+
+Tabs.propTypes = {};
+
+Tabs.defaultProps = {};
+
+export default Tabs;
