a3221e093f23038ee39c4bdf06bd791a02e9fa65
ggdaltoso <ggdaltoso@gmail.com>
2019-01-23 22:01:33 -0200

    Functional components

--- components/Tabs/Tab.jsx
+++ components/Tabs/Tab.jsx
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const NavItem = styled.li``;
+
 class Tab extends Component {
   constructor(props) {
     super(props);
   }
 
+  onClick = () => {
+    const { onClick } = this.props;
+    onClick(this);
+  };
+
   render() {
-    return '';
+    const { activeTab, title } = this.props;
+
+    return (
+      <NavItem activeTab={activeTab} onClick={this.onClick}>
+        {title}
+      </NavItem>
+    );
   }
 }
 
-Tab.propTypes = {};
-
-Tab.defaultProps = {};
+Tab.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Tab;
--- components/Tabs/Tabs.jsx
+++ components/Tabs/Tabs.jsx
@@ -2,18 +2,65 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import Tab from './Tab';
+
+const Navbar = styled.nav``;
+
+const renderIf = (conditional, renderFn) => conditional && renderFn();
+
 class Tabs extends Component {
   constructor(props) {
     super(props);
+
+    const firstTab = React.Children.toArray(children)[0];
+
+    this.state = {
+      activeTab: firstTab.props.title,
+    };
   }
 
+  onTabClick = ({ props: title }) => {
+    this.setState({ activeTab: title });
+  };
+
   render() {
-    return '';
+    const { children } = this.props;
+
+    return (
+      <React.Fragment>
+        <Navbar>
+          <ol>
+            {React.Children.map(children, child => {
+              const { title } = child.props;
+
+              return (
+                <Tab
+                  key={title}
+                  title={title}
+                  activeTab={activeTab}
+                  onClick={this.onTabClick}
+                />
+              );
+            })}
+          </ol>
+        </Navbar>
+
+        {React.Children.map(children, child =>
+          renderIf(
+            children.indexOf(child).props.title === activeTab,
+            () => child,
+          ),
+        )}
+      </React.Fragment>
+    );
   }
 }
 
-Tabs.propTypes = {};
-
-Tabs.defaultProps = {};
+Tabs.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
 
 export default Tabs;
