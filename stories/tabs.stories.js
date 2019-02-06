9ce22bc0a4069b63b5e41d484cae0f9ce82b41d2
ggdaltoso <ggdaltoso@gmail.com>
2019-02-05 23:41:18 -0200

    update docs

--- stories/tabs.stories.js
+++ stories/tabs.stories.js
@@ -7,40 +7,50 @@ import Fieldset from '../components/Fieldset';
 import Checkbox from '../components/Checkbox';
 import Dropdown from '../components/Dropdown';
 
-storiesOf('Tabs', module).add('default', () => (
-  <Tabs style={{ width: 350 }} activeTab="Compatibility">
-    <Tab title="General" disabled />
-    <Tab title="Compatibility">
-      <p style={{ marginTop: 0, marginBottom: '1.6em' }}>
-        If you have problem with this program and it worked correctly on an
-        earlier version of Windows, select the compatibility m ode that mathes
-        that earlier version.
-      </p>
+const code = `<Tabs activeTab="Compatibility">
+<Tab title="General" disabled />
+  <Tab title="Compatibility">
+    . . .
+  </Tab>
+</Tabs>`;
 
-      <Fieldset legend="Compatibility mode" style={{ marginBottom: '1.6em' }}>
-        <Checkbox checked={true}>
-          Run this program in compatibility mode for:
-        </Checkbox>
-        <Dropdown style={{ width: 200 }} options={['Windows 95']} />
-      </Fieldset>
+storiesOf('Tabs', module)
+  .addDecorator(withClippy)
+  .addParameters({ code })
+  .add('default', () => (
+    <Tabs style={{ width: 350 }} activeTab="Compatibility">
+      <Tab title="General" disabled />
+      <Tab title="Compatibility">
+        <p style={{ marginTop: 0, marginBottom: '1.6em' }}>
+          If you have problem with this program and it worked correctly on an
+          earlier version of Windows, select the compatibility m ode that mathes
+          that earlier version.
+        </p>
 
-      <Fieldset legend="Display Settings">
-        <Checkbox checked={false}>Run in 256 colors</Checkbox>
-        <Checkbox checked={false}>Run in 640 x 480 screen resolution</Checkbox>
-        <Checkbox checked={false}>Disable visual themes</Checkbox>
-      </Fieldset>
+        <Fieldset legend="Compatibility mode" style={{ marginBottom: '1.6em' }}>
+          <Checkbox checked={true}>
+            Run this program in compatibility mode for:
+          </Checkbox>
+          <Dropdown style={{ width: 200 }} options={['Windows 95']} />
+        </Fieldset>
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>
-        Learn more about <a href="#">program compatibility.</a>
-      </p>
-    </Tab>
-  </Tabs>
-));
+        <Fieldset legend="Display Settings">
+          <Checkbox>Run in 256 colors</Checkbox>
+          <Checkbox>Run in 640 x 480 screen resolution</Checkbox>
+          <Checkbox>Disable visual themes</Checkbox>
+        </Fieldset>
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+          Learn more about <a href="#">program compatibility.</a>
+        </p>
+      </Tab>
+    </Tabs>
+  ));
