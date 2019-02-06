fdce4e5466afd58fbc6d45e86316040eac42c781
ggdaltoso <ggdaltoso@gmail.com>
2019-02-05 23:35:51 -0200

    Tab story

--- stories/tabs.stories.js
+++ stories/tabs.stories.js
@@ -3,12 +3,44 @@ import { storiesOf } from '@storybook/react';
 import withClippy from '../.storybook/src/clippy-addon/clippy-addon';
 
 import { Tabs, Tab } from '../components/Tabs';
+import Fieldset from '../components/Fieldset';
+import Checkbox from '../components/Checkbox';
+import Dropdown from '../components/Dropdown';
 
 storiesOf('Tabs', module).add('default', () => (
-  <Tabs>
-    <Tab title="Background">1</Tab>
-    <Tab title="Screen Saver">2</Tab>
-    <Tab title="Appearance">3</Tab>
-    <Tab title="Settings">4</Tab>
+  <Tabs style={{ width: 350 }} activeTab="Compatibility">
+    <Tab title="General" disabled />
+    <Tab title="Compatibility">
+      <p style={{ marginTop: 0, marginBottom: '1.6em' }}>
+        If you have problem with this program and it worked correctly on an
+        earlier version of Windows, select the compatibility m ode that mathes
+        that earlier version.
+      </p>
+
+      <Fieldset legend="Compatibility mode" style={{ marginBottom: '1.6em' }}>
+        <Checkbox checked={true}>
+          Run this program in compatibility mode for:
+        </Checkbox>
+        <Dropdown style={{ width: 200 }} options={['Windows 95']} />
+      </Fieldset>
+
+      <Fieldset legend="Display Settings">
+        <Checkbox checked={false}>Run in 256 colors</Checkbox>
+        <Checkbox checked={false}>Run in 640 x 480 screen resolution</Checkbox>
+        <Checkbox checked={false}>Disable visual themes</Checkbox>
+      </Fieldset>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>
+        Learn more about <a href="#">program compatibility.</a>
+      </p>
+    </Tab>
   </Tabs>
 ));
