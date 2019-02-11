8898d5e5c3eee35260a0c65ca635b74c91a3e955
Allysson Santos <aallysson0@gmail.com>
2019-02-11 21:44:21 -0200

    Create RadioButton story

--- stories/radiobutton.stories.js
+++ stories/radiobutton.stories.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import withClippy from '../.storybook/src/clippy-addon/clippy-addon';
 
+import { Tab, Tabs } from '../components/Tabs';
+
 import RadioButton from '../components/RadioButton';
 
 const code = '<RadioButton />';
@@ -9,4 +11,14 @@ const code = '<RadioButton />';
 storiesOf('RadioButton', module)
   .addDecorator(withClippy)
   .addParameters({ code })
-  .add('default', () => <RadioButton />);
+  .add('default', () => (
+    <>
+      <RadioButton name="working">Working</RadioButton>
+      <RadioButton name="working">Working</RadioButton>
+      <RadioButton checked>Checked</RadioButton>
+      <RadioButton disabled>Disabled</RadioButton>
+      <RadioButton checked disabled>
+        Checked & Disabled
+      </RadioButton>
+    </>
+  ));
