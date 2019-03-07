4f4016d02c918c7753f3bc2dfae860a70390a519
ggdaltoso <ggdaltoso@gmail.com>
2019-03-06 22:17:29 -0300

    chore(build): script helper to improve the dist package
    re #56

---
+++ scripts/copy-files.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+import pkg from '../package.json';
+
+const copyFile = file => {
+  const buildPath = path.resolve(__dirname, '../dist/', path.basename(file));
+  fs.copyFile(file, buildPath, () =>
+    console.log(`Copied ${file} to ${buildPath}`),
+  );
+};
+
+const createPackageJson = () => {
+  const {
+    scripts,
+    devDependencies,
+    'lint-staged': lintStaged,
+    jest,
+    ...packageDataOther
+  } = pkg;
+
+  const newPackageData = {
+    ...packageDataOther,
+    main: './index.js',
+    private: false,
+  };
+
+  const buildPath = path.resolve(__dirname, '../dist/package.json');
+
+  fs.writeFileSync(buildPath, JSON.stringify(newPackageData, null, 2), 'utf8');
+  console.log(`Created package.json in ${buildPath}`);
+
+  return newPackageData;
+};
+
+const run = () => {
+  ['CONTRIBUTING.md', 'README.md', 'CODE_OF_CONDUCT.md'].map(copyFile);
+  createPackageJson();
+};
+
+run();
