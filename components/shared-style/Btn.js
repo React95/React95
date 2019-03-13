13fe7c04a3e3a3e17aeca6f86292a32f5380e34e
Allysson Santos <aallysson0@gmail.com>
2019-03-13 19:08:55 -0300

    refactor(Button,RadioButton): Add focus style and refactor btn style

--- components/shared-style/Btn.js
+++ components/shared-style/Btn.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export default styled.button`
   background-color: #c3c7cb;
-  padding: 6px 20px 6px;
+  padding: 7px 20px 5px;
   border: none;
 
   font-size: 12px;
 
   box-shadow: inset 1px 1px 0px 1px #ffffff, inset 0 0 0 1px #868a8e,
-    1px 1px 0 1px #000;
+    1px 1px 0 0px #000;
 
   &:disabled {
     color: #868a8e;
@@ -23,7 +23,7 @@ export default styled.button`
   }
 
   &:active {
-    padding: 7px 20px 6px;
+    padding: 8px 20px 5px;
 
     outline: 1px dotted #000;
     outline-offset: -5px;
