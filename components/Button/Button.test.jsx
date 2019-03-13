949d85982ed0a2fcb7cd806b4852821d7dd02a05
Allysson Santos <aallysson0@gmail.com>
2019-03-13 08:56:06 -0300

    test(Button): Button unit tests
    #70

--- components/Button/Button.test.jsx
+++ components/Button/Button.test.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import Button from './Button';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe('<Button />', () => {
   describe('Snapshots', () => {
     it('should match snapshot', () => {
       expect(renderer.create(<Button />).toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('value prop', () => {
+    it('should display value as Button children', () => {
+      const ShallowButton = shallow(<Button value="Cancel" />);
+
+      expect(ShallowButton.text()).toBe('Cancel');
+    });
+  });
+
+  describe('onClick prop', () => {
+    it('should call onClick function when Button is clicked', () => {
+      const onClickMock = jest.fn();
+      const ShallowButton = shallow(<Button onClick={onClickMock} />);
+
+      ShallowButton.simulate('click');
+
+      expect(onClickMock).toHaveBeenCalled();
     });
   });
 });
