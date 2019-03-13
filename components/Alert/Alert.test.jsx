c92166d0ad1aa4313f3055210143c3ec917506ab
Allysson Santos <aallysson0@gmail.com>
2019-03-13 19:24:13 -0300

    test(Alert): Add unit test for closeAlert prop
    #70

--- components/Alert/Alert.test.jsx
+++ components/Alert/Alert.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Alert from './Alert';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
 describe('<Alert />', () => {
   describe('Snapshots', () => {
@@ -26,6 +27,23 @@ describe('<Alert />', () => {
           .create(<Alert title="Info" type="info" message="Info" />)
           .toJSON()
       ).toMatchSnapshot();
+    });
+  });
+
+  describe('closeAlert prop', () => {
+    it('should call closeAlert when modal "OK" button is pressed', () => {
+      const closeAlertMock = jest.fn();
+      const ShallowAlert = mount(
+        <Alert
+          title="closeAlert"
+          message="closeAlert"
+          closeAlert={closeAlertMock}
+        />
+      );
+
+      ShallowAlert.find('Button').simulate('click');
+
+      expect(closeAlertMock).toHaveBeenCalled();
     });
   });
 });
