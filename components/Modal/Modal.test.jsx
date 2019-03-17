3a08b7b7ff5111d8be18e0aeadaaa72ada7263f8
Allysson Santos <aallysson0@gmail.com>
2019-03-17 14:12:58 -0300

    test(Modal): Refactor test to use react-testing-library

--- components/Modal/Modal.test.jsx
+++ components/Modal/Modal.test.jsx
@@ -1,130 +1,124 @@
 import React from 'react';
 import Modal from './Modal';
 import List from '../List';
-import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { render, cleanup, fireEvent } from 'react-testing-library';
+
+afterEach(cleanup);
 
 describe('<Modal />', () => {
   describe('Snapshots', () => {
     it('should match snapshot', () => {
-      expect(
-        renderer
-          .create(
-            <Modal
-              icon="bat"
-              title="file.bat"
-              buttons={[{ value: 'Ok' }, { value: 'Cancel' }]}
-              menu={[
-                {
-                  name: 'File',
-                  list: (
-                    <List>
-                      <List.Item>Exit</List.Item>
-                    </List>
-                  ),
-                },
-                {
-                  name: 'Edit',
-                  list: (
-                    <List>
-                      <List.Item>Copy</List.Item>
-                    </List>
-                  ),
-                },
-              ]}
-            >
-              Hello
-            </Modal>
-          )
-          .toJSON()
-      ).toMatchSnapshot();
+      const { container } = render(
+        <Modal
+          icon="bat"
+          title="file.bat"
+          buttons={[{ value: 'Ok' }, { value: 'Cancel' }]}
+          menu={[
+            {
+              name: 'File',
+              list: (
+                <List>
+                  <List.Item>Exit</List.Item>
+                </List>
+              ),
+            },
+            {
+              name: 'Edit',
+              list: (
+                <List>
+                  <List.Item>Copy</List.Item>
+                </List>
+              ),
+            },
+          ]}
+        >
+          Hello
+        </Modal>
+      );
+      expect(container).toMatchSnapshot();
     });
 
     it('should match snapshot with buttonsAligment prop equals center', () => {
-      expect(
-        renderer
-          .create(
-            <Modal
-              icon="bat"
-              title="file.bat"
-              buttons={[{ value: 'Ok' }, { value: 'Cancel' }]}
-              buttonsAlignment="center"
-            >
-              Hello
-            </Modal>
-          )
-          .toJSON()
-      ).toMatchSnapshot();
+      const { container } = render(
+        <Modal
+          icon="bat"
+          title="file.bat"
+          buttons={[{ value: 'Ok' }, { value: 'Cancel' }]}
+          buttonsAlignment="center"
+        >
+          Hello
+        </Modal>
+      );
+      expect(container).toMatchSnapshot();
     });
 
     it('should match snapshot with buttonsAligment prop equals flex-start', () => {
-      expect(
-        renderer
-          .create(
-            <Modal
-              icon="bat"
-              title="file.bat"
-              buttons={[{ value: 'Ok' }, { value: 'Cancel' }]}
-              buttonsAlignment="flex-start"
-            >
-              Hello
-            </Modal>
-          )
-          .toJSON()
-      ).toMatchSnapshot();
+      const { container } = render(
+        <Modal
+          icon="bat"
+          title="file.bat"
+          buttons={[{ value: 'Ok' }, { value: 'Cancel' }]}
+          buttonsAlignment="flex-start"
+        >
+          Hello
+        </Modal>
+      );
+      expect(container).toMatchSnapshot();
     });
   });
 
   describe('closeModal prop', () => {
-    it('should call closeModal when Modal close button is clicked', () => {
+    it('should call closeModal when Modal close button is clicked', async () => {
       const closeModalMock = jest.fn();
-      const MountedModal = mount(
+      const { getByText } = render(
         <Modal icon="bat" title="file.bat" closeModal={closeModalMock}>
           Hello
         </Modal>
       );
 
-      MountedModal.find('Option')
-        .at(1)
-        .simulate('click');
+      fireEvent.click(getByText('x'));
 
       expect(closeModalMock).toHaveBeenCalled();
     });
   });
 
   describe('Modal action buttons', () => {
-    it('should display button value correctly', () => {
-      const buttonValue = 'button text';
-      const MountedModal = mount(
-        <Modal icon="bat" title="file.bat" buttons={[{ value: buttonValue }]}>
+    it('should display button text correctly', () => {
+      const buttonText = 'button text';
+      const { getByText } = render(
+        <Modal icon="bat" title="file.bat" buttons={[{ value: buttonText }]}>
           Hello
         </Modal>
       );
 
-      expect(MountedModal.find('Btn').text()).toBe(buttonValue);
+      expect(getByText(buttonText)).toBeInTheDocument();
     });
 
     it('should display more than one button', () => {
-      const MountedModal = mount(
+      const { getAllByText } = render(
         <Modal
           icon="bat"
           title="file.bat"
-          buttons={[{ value: 'OK' }, { value: 'Apply' }, { value: 'Cancel' }]}
+          buttons={[
+            { value: 'button' },
+            { value: 'button' },
+            { value: 'button' },
+          ]}
         >
           Hello
         </Modal>
       );
 
-      expect(MountedModal.find('Btn').length).toBe(3);
+      expect(getAllByText('button').length).toBe(3);
     });
 
     it('should call onClick function when Modal action button is clicked', () => {
       const onClickMock = jest.fn();
-      const MountedModal = mount(
+      const { getByText } = render(
         <Modal buttons={[{ value: 'Ok', onClick: onClickMock }]}>Hello</Modal>
       );
 
-      MountedModal.find('Btn').simulate('click');
+      fireEvent.click(getByText('Ok'));
 
       expect(onClickMock).toHaveBeenCalled();
     });
@@ -132,12 +126,11 @@ describe('<Modal />', () => {
 
   describe('Menus', () => {
     it('should display menu name correctly', () => {
-      const menuText = 'Text';
-      const MountedModal = mount(
+      const { getByText } = render(
         <Modal
           menu={[
             {
-              name: menuText,
+              name: 'Menu Text',
               list: (
                 <List>
                   <List.Item>Exit</List.Item>
@@ -150,19 +143,18 @@ describe('<Modal />', () => {
         </Modal>
       );
 
-      expect(MountedModal.find('MenuItem').text()).toBe(menuText);
+      expect(getByText('Menu Text')).toBeInTheDocument();
     });
 
     it('should display menu list correctly', () => {
-      const menuListText = 'Exit';
-      const MountedModal = mount(
+      const { getByText } = render(
         <Modal
           menu={[
             {
               name: 'Edit',
               list: (
                 <List>
-                  <List.Item>{menuListText}</List.Item>
+                  <List.Item>Exit</List.Item>
                 </List>
               ),
             },
@@ -172,10 +164,9 @@ describe('<Modal />', () => {
         </Modal>
       );
 
-      MountedModal.find('MenuItem').simulate('mousedown');
+      fireEvent.mouseDown(getByText('Edit'));
 
-      expect(MountedModal.find(List).exists()).toBe(true);
-      expect(MountedModal.find(List.Item).text()).toBe(menuListText);
+      expect(getByText('Exit')).toBeInTheDocument();
     });
   });
 });
