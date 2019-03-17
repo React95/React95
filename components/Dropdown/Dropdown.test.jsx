3db0d3c388daf282b45cc3ec4f6f63d628724abd
Allysson Santos <aallysson0@gmail.com>
2019-03-17 14:22:45 -0300

    test(Dropdown): Refactor test to use react-testing-library

--- components/Dropdown/Dropdown.test.jsx
+++ components/Dropdown/Dropdown.test.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import Dropdown from './Dropdown';
-import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { render } from 'react-testing-library';
 
 describe('<Dropdown />', () => {
   const options = ['', 'C:\\Windows', 'C:\\Windows/System32'];
 
   describe('Snapshots', () => {
     it('should match snapshot with options prop', () => {
-      expect(
-        renderer.create(<Dropdown options={options} />).toJSON()
-      ).toMatchSnapshot();
+      const { container } = render(<Dropdown options={options} />);
+      expect(container).toMatchSnapshot();
     });
   });
 
   describe('options prop', () => {
     it('should display correct number of options', () => {
-      const MountedDropown = mount(<Dropdown options={options} />);
-      const dropDownOptions = MountedDropown.find('option');
+      const { container } = render(<Dropdown options={options} />);
+      const dropDownOptions = container.querySelectorAll('option');
 
       expect(dropDownOptions.length).toBe(3);
     });
 
     it('should display correct option text', () => {
-      const MountedDropown = mount(<Dropdown options={options} />);
-      const dropDownOptions = MountedDropown.find('option');
+      const { getByText } = render(<Dropdown options={options} />);
 
-      expect(dropDownOptions.at(0).text()).toBe(options[0]);
-      expect(dropDownOptions.at(1).text()).toBe(options[1]);
-      expect(dropDownOptions.at(2).text()).toBe(options[2]);
+      expect(getByText(options[0])).toBeInTheDocument();
+      expect(getByText(options[1])).toBeInTheDocument();
+      expect(getByText(options[2])).toBeInTheDocument();
     });
   });
 });
