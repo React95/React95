428f64aca1d56f9411af567f24ec2a585ea67aea
Andre Loureiro <andreloureiroo@gmail.com>
2019-04-14 00:35:28 +0200

    refactor(tree): Removes comments

--- components/Tree/Tree.test.jsx
+++ components/Tree/Tree.test.jsx
@@ -68,33 +68,4 @@ describe('<Tree />', () => {
       expect(onClickMock).toHaveBeenCalled();
     });
   })
-
-  // describe('Active Tab', () => {
-  //   const TabComponent = () =>
-  //     render(
-  //       <Tabs>
-  //         <Tab title="first">
-  //           <p>first text</p>
-  //         </Tab>
-  //         <Tab title="second">
-  //           <p>second text</p>
-  //         </Tab>
-  //       </Tabs>
-  //     );
-
-  //   it('should have first Tab active', () => {
-  //     const { queryByText } = TabComponent();
-  //     expect(queryByText('first text')).toBeInTheDocument();
-  //   });
-
-  //   it('should active second Tab when Tab is clicked', () => {
-  //     const { queryByText } = TabComponent();
-  //     expect(queryByText('first text')).toBeInTheDocument();
-
-  //     fireEvent.click(queryByText('second'));
-
-  //     expect(queryByText('first text')).not.toBeInTheDocument();
-  //     expect(queryByText('second text')).toBeInTheDocument();
-  //   });
-  // });
 });
