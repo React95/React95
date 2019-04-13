49f4174feed28e3a03f875eb6072172d279f763d
Andre Loureiro <andreloureiroo@gmail.com>
2019-04-14 00:25:31 +0200

    feat(tree): Adds the Tree component

---
+++ components/Tree/Tree.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Node, { icons } from './Node';
+
+const TreeParent = styled.ul`
+  padding: 0px;
+`;
+
+const Tree = ({ data }) => (
+  <TreeParent>
+    {data.map(dataNode => (
+      <Node key={dataNode.id} {...dataNode} />
+    ))}
+  </TreeParent>
+);
+
+Tree.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape(Node.propTypes)
+  )
+};
+
+Tree.defaultProps = {
+  data: []
+};
+
+Tree.icons = icons;
+
+export default Tree;
