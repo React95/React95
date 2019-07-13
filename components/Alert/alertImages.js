e1a84220b9d0cbf7c9f31f34e663c025362a823c
Allysson Santos <aallysson0@gmail.com>
2019-07-13 14:37:27 -0300

    feat(Alert): Add question alert option
    close #98

--- components/Alert/alertImages.js
+++ components/Alert/alertImages.js
@@ -1,5 +1,6 @@
 import dialogError from '../shared/assets/pattern/dialog-error.png';
 import dialogInfo from '../shared/assets/pattern/dialog-info.png';
+import dialogQuestion from '../shared/assets/pattern/dialog-question.png';
 import dialogWarning from '../shared/assets/pattern/dialog-warning.png';
 
-export { dialogError, dialogInfo, dialogWarning };
+export { dialogError, dialogInfo, dialogQuestion, dialogWarning };
