3b96b7f3fd196dfc3303bbd5be8900c8714dfbdc
ggdaltoso <ggdaltoso@gmail.com>
2019-09-02 14:52:33 -0300

    build(babel): update the build process and publish configuration according to lerna configs
    re #116

--- packages/core/stories/range.stories.js
+++ packages/core/stories/range.stories.js
@@ -2,8 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Range from "../components/Range";
 
-const code = "<Range />";
-
 storiesOf("Range", module).add("default", () => (
   <Range style={{ width: 100 }} />
 ));
