073527d031c3158a62bc455aae0720207035d618
ggdaltoso <ggdaltoso@gmail.com>
2020-01-20 21:43:58 -0300

    improvement(tabs): add @xstyled

--- packages/core/components/Tabs/Tab.jsx
+++ packages/core/components/Tabs/Tab.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "@xstyled/styled-components";
-import { th } from "@xstyled/system";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from '@xstyled/styled-components';
+import { th } from '@xstyled/system';
 
 const NavItem = styled.li`
   list-style: none;
   padding: 3 6;
   background-color: grays.2;
-  box-shadow: inset 0px 1px 0px 0px ${th("colors.white")},
-    -2px 1px 0 -1px ${th("colors.grays.2")},
-    -3px 1px 0px -1px ${th("colors.white")},
-    -2px 0px 0px -1px ${th("colors.white")},
-    2px 1px 0 -1px ${th("colors.grays.3")}, 2px 0px 0 -1px ${th("colors.black")},
-    3px 1px 0 -1px ${th("colors.black")};
+  box-shadow: inset 0 1px 0 0 ${th('colors.white')},
+    -2px 1px 0 -1px ${th('colors.grays.2')},
+    -3px 1px 0 -1px ${th('colors.white')}, -2px 0 0 -1px ${th('colors.white')},
+    2px 1px 0 -1px ${th('colors.grays.3')}, 2px 0 0 -1px ${th('colors.black')},
+    3px 1px 0 -1px ${th('colors.black')};
   margin-right: 2;
   margin-left: 2;
 
@@ -22,24 +21,28 @@ const NavItem = styled.li`
 
   ${props =>
     props.active &&
-    `
-      margin-top: -2px;
-      margin-left: -2px;
-      margin-bottom: 1px;
+    css`
+      margin-top: -${th('space.2')}px;
+      margin-left: -${th('space.2')}px;
+      margin-bottom: 1;
 
       &:first-child {
-        margin-left: 2px;
+        margin-left: 2;
       }
 
-      box-shadow: inset 0px 1px 0px 0px #ffffff, -2px 1px 0 -1px #c3c7cb,
-        -0.5px 3px 0 0.5px #c3c7cb, -2px 2px 0px 0px #fff,
-        -2px 0px 0px -1px #ffffff, 1px 2px 0 0px #868a8e, 2px 0px 0 -1px #000,
-        2px 2px 0 0px #000;
+      box-shadow: inset 0 1px 0 0 ${th('colors.white')},
+        -2px 1px 0 -1px ${th('colors.grays.2')},
+        -0.5px 3px 0 0.5px ${th('colors.grays.2')},
+        -2px 2px 0 0 ${th('colors.white')}, -2px 0 0 -1px ${th('colors.white')},
+        1px 2px 0 0 ${th('colors.grays.3')}, 2px 0 0 -1px ${th('colors.black')},
+        2px 2px 0 0 ${th('colors.black')};
 
       & + li {
         margin-left: 0;
-        box-shadow: inset 0px 1px 0px 0px #ffffff, 2px 1px 0 -1px #868a8e,
-          2px 0px 0 -1px #000, 3px 1px 0 -1px #000;
+        box-shadow: inset 0 1px 0 0 ${th('colors.white')},
+          2px 1px 0 -1px ${th('colors.grays.3')},
+          2px 0 0 -1px ${th('colors.black')},
+          3px 1px 0 -1px ${th('colors.black')};
       }
     `}
 `;
@@ -52,11 +55,11 @@ const Tab = ({ activeTab, title, ...rest }) => (
 
 Tab.propTypes = {
   activeTab: PropTypes.string,
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 };
 
 Tab.defaultProps = {
-  activeTab: undefined
+  activeTab: undefined,
 };
 
 export default Tab;
