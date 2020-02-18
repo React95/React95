92c881f14408948d047dcc3fa3f1843fa0b901d8
ggdaltoso <ggdaltoso@gmail.com>
2020-02-17 21:49:43 -0300

    feat(theme): add more spaces

--- packages/core/components/React95ThemeProvider/theme.js
+++ packages/core/components/React95ThemeProvider/theme.js
@@ -6,7 +6,29 @@ const colors = {
   green: '#5aa',
 };
 
-const space = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const space = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+];
 
 const borderWidths = [0, 1];
 const borderStyles = ['none', 'solid'];
