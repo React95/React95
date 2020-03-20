1f03b36314fbb606c55a88aadaa5d0c249a849af
ggdaltoso <ggdaltoso@gmail.com>
2020-03-20 15:30:29 -0300

    test(jest): add display name and update snapshots

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
+  displayName: 'core',
   setupFilesAfterEnv: [
     'babel-polyfill',
     '@testing-library/jest-dom/extend-expect',
     'jest-styled-components',
-    '@testing-library/react/cleanup-after-each',
   ],
 };
