881831bdf9078a638f24dd3fdf1c8aff2b597292
ggdaltoso <ggdaltoso@gmail.com>
2020-03-26 09:03:55 -0300

    feat(modalcontext): add defaultValues to createContext

--- packages/core/components/Modal/ModalContext.js
+++ packages/core/components/Modal/ModalContext.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-unused-vars */
 import { createContext } from 'react';
 
-const ModalContext = createContext({ windows: [] });
+const ModalContext = createContext({
+  windows: [],
+  addWindows: ({ icon, title }) => {},
+  removeWindows: title => {},
+  setActiveWindow: title => {},
+  activeWindow: '',
+});
 
 export default ModalContext;
