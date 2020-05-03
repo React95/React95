fe9a1030017ae35050feeb807f4a695d1de893a7
ggdaltoso <ggdaltoso@gmail.com>
2020-05-02 21:27:52 -0300

    docs(icons): better icons visualization

--- packages/core/stories/icons.stories.js
+++ packages/core/stories/icons.stories.js
@@ -16,6 +16,18 @@ const TableWrapper = styled.div`
   .ReactVirtualized__Table__rowColumn {
     margin-right: 0;
   }
+
+  .ReactVirtualized__Table__headerRow {
+    border-bottom: 1px solid;
+  }
+
+  .ReactVirtualized__Table__headerColumn,
+  .ReactVirtualized__Table__rowColumn {
+    margin-left: 0;
+    margin-right: 0;
+    border-right: 1px solid;
+    text-align: center;
+  }
 `;
 
 const list = Object.keys(icons).map((icon) => ({
@@ -23,27 +35,46 @@ const list = Object.keys(icons).map((icon) => ({
 }));
 
 storiesOf('Icon', module).add('default', () => (
-  <Frame p={4} width={387}>
-    As an{' '}
-    <a href="https://en.wikipedia.org/wiki/ICO_(file_format)" target="_blank">
-      ico file format
-    </a>{' '}
-    is, we have some sizes available for some icons. Here are only 16x16 and
-    32x32 dimensions (due performance issues) for our{' '}
-    {Object.keys(icons).length} icons:
+  <Frame p={4} width={875}>
+    <p>
+      As an{' '}
+      <a href="https://en.wikipedia.org/wiki/ICO_(file_format)" target="_blank">
+        ico file format
+      </a>{' '}
+      is, we have some sizes available for some icons. Here are some dimensions.
+      There is some icons available in 256x256, like LIGHTS_99, that is not in
+      this pretty visualization.
+    </p>
+    <p>We have, actually, {Object.keys(icons).length} icons:</p>
     <Frame bg="white" boxShadow="in" mt={4}>
       <TableWrapper>
         <Table
-          width={380}
+          width={868}
           height={700}
           headerHeight={20}
+          headerStyle={{ padding: 4 }}
           rowHeight={130}
           rowCount={list.length}
           rowGetter={({ index }) => list[index]}
+          rowStyle={{ alignItems: 'stretch' }}
         >
-          <Column label="Name" dataKey="name" width={100} />
           <Column
-            width={32}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            label="Name"
+            dataKey="name"
+            width={100}
+          />
+          <Column
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            width={128}
             label={16}
             dataKey="name"
             cellRenderer={({ cellData }) => (
@@ -56,8 +87,33 @@ storiesOf('Icon', module).add('default', () => (
               />
             )}
           />
+
           <Column
-            width={32}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            width={128}
+            label={24}
+            dataKey="name"
+            cellRenderer={({ cellData }) => (
+              <Icon
+                size={24}
+                width={24}
+                height={24}
+                name={cellData}
+                style={{ display: 'inline-block' }}
+              />
+            )}
+          />
+          <Column
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            width={128}
             label={32}
             dataKey="name"
             cellRenderer={({ cellData }) => (
@@ -71,7 +127,31 @@ storiesOf('Icon', module).add('default', () => (
             )}
           />
           <Column
-            width={64}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            width={128}
+            label={48}
+            dataKey="name"
+            cellRenderer={({ cellData }) => (
+              <Icon
+                size={48}
+                width={48}
+                height={48}
+                name={cellData}
+                style={{ display: 'inline-block' }}
+              />
+            )}
+          />
+          <Column
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            width={128}
             label={64}
             dataKey="name"
             cellRenderer={({ cellData }) => (
@@ -85,6 +165,11 @@ storiesOf('Icon', module).add('default', () => (
             )}
           />
           <Column
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
             width={128}
             label={128}
             dataKey="name"
