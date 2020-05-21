9a23fb6a53031c148c1dd6b372781f911c9b41da
ggdaltoso <ggdaltoso@gmail.com>
2020-05-21 14:25:41 -0300

    test(video): add test for play, pause, and stop events

--- packages/core/components/Video/Video.test.jsx
+++ packages/core/components/Video/Video.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { waitRender } from '../shared/test/utils';
+import { waitRender, fireEvent, act } from '../shared/test/utils';
 import Video from './Video';
 
 describe('<Video />', () => {
@@ -9,6 +9,37 @@ describe('<Video />', () => {
         <Video src="foo/bar/some_video.mp4" />,
       );
       expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('events', () => {
+    it('should play, pause, and stop', async () => {
+      const playStub = jest
+        .spyOn(window.HTMLMediaElement.prototype, 'play')
+        .mockImplementation(() => {});
+      const pauseStub = jest
+        .spyOn(window.HTMLMediaElement.prototype, 'pause')
+        .mockImplementation(() => {});
+
+      const { container } = await waitRender(
+        <Video src="foo/bar/some_video.mp4" />,
+      );
+
+      const video = container.querySelector('video');
+      act(() => video.dispatchEvent(new window.Event('loadeddata')));
+
+      const [playBtn, stopBtn] = container.querySelectorAll('button');
+
+      // Play
+      fireEvent.click(playBtn);
+      // Pause
+      fireEvent.click(playBtn);
+      // Stop
+      fireEvent.click(stopBtn);
+
+      expect(playStub).toHaveBeenCalled();
+      // Pause + Stop
+      expect(pauseStub).toHaveBeenCalledTimes(2);
     });
   });
 });
