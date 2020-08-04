9964d4a744049d148f47d7c725565bb95c8b9aaf
ggdaltoso <ggdaltoso@gmail.com>
2020-08-04 09:14:20 -0300

    refactor(stories): remove class from ModalStory

--- packages/core/stories/modal.stories.js
+++ packages/core/stories/modal.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
 import withClippy from '../.storybook/src/clippy-addon/clippy-addon';
@@ -7,64 +7,52 @@ import { Modal } from '../components/Modal';
 import Button from '../components/Button';
 import List from '../components/List';
 
-class ModalStory extends React.Component {
-  constructor(props) {
-    super(props);
+const ModalStory = ({ children }) => {
+  const [showModal, toggleShowModal] = useState(true);
 
-    this.state = {
-      showModal: true,
-    };
-  }
+  const handleOpenModal = () => toggleShowModal(true);
+  const handleCloseModal = () => toggleShowModal(false);
+  const handleButtonClick = e => alert(e.target.value);
 
-  handleOpenModal = () => this.setState({ showModal: true });
-
-  handleCloseModal = () => this.setState({ showModal: false });
-
-  handleButtonClick = e => alert(e.target.value);
-
-  render() {
-    const { children } = this.props;
-    const { showModal } = this.state;
-    return (
-      <>
-        <Button onClick={this.handleOpenModal}>Trigger Modal</Button>
-        {showModal && (
-          <Modal
-            width={300}
-            height={200}
-            icon="computer"
-            title="Browse"
-            closeModal={this.handleCloseModal}
-            buttons={[
-              { value: 'Ok', onClick: this.handleButtonClick },
-              { value: 'Cancel', onClick: this.handleButtonClick },
-            ]}
-            menu={[
-              {
-                name: 'File',
-                list: (
-                  <List>
-                    <List.Item onClick={this.handleCloseModal}>Exit</List.Item>
-                  </List>
-                ),
-              },
-              {
-                name: 'Edit',
-                list: (
-                  <List>
-                    <List.Item>Copy</List.Item>
-                  </List>
-                ),
-              },
-            ]}
-          >
-            {children}
-          </Modal>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Button onClick={handleOpenModal}>Trigger Modal</Button>
+      {showModal && (
+        <Modal
+          width={300}
+          height={200}
+          icon="computer"
+          title="Browse"
+          closeModal={handleCloseModal}
+          buttons={[
+            { value: 'Ok', onClick: handleButtonClick },
+            { value: 'Cancel', onClick: handleButtonClick },
+          ]}
+          menu={[
+            {
+              name: 'File',
+              list: (
+                <List>
+                  <List.Item onClick={handleCloseModal}>Exit</List.Item>
+                </List>
+              ),
+            },
+            {
+              name: 'Edit',
+              list: (
+                <List>
+                  <List.Item>Copy</List.Item>
+                </List>
+              ),
+            },
+          ]}
+        >
+          {children}
+        </Modal>
+      )}
+    </>
+  );
+};
 
 const code = `<Modal
   icon="computer"
