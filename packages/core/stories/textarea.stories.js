e4ea5ec6d4bc7acb86d59510a693e7d50b2a4766
ggdaltoso <ggdaltoso@gmail.com>
2020-08-04 09:17:23 -0300

    refactor(stories): remove class from TextArea

--- packages/core/stories/textarea.stories.js
+++ packages/core/stories/textarea.stories.js
@@ -1,25 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import withClippy from '../.storybook/src/clippy-addon/clippy-addon';
 
 import TextArea from '../components/TextArea';
 
-class TextStory extends React.Component {
-  constructor(props) {
-    super(props);
+const TextStory = () => {
+  const [text, setValue] = useState('');
 
-    this.state = {
-      value: '',
-    };
-  }
+  const handleChange = ({ target: { value } }) => setValue(value);
 
-  handleChange = ({ target: { value } }) => this.setState({ value });
-
-  render() {
-    const { value } = this.state;
-    return <TextArea value={value} onChange={this.handleChange} />;
-  }
-}
+  return <TextArea value={text} onChange={handleChange} />;
+};
 
 const code = '<TextArea rows={10} cols={50} />';
 
