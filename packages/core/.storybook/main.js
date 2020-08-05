e782e14c4a1e0f96774e0ab0a435e9bf64f189dc
ggdaltoso <ggdaltoso@gmail.com>
2020-08-05 09:29:34 -0300

    chore(storybook): add support for ts files

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -5,15 +5,31 @@ module.exports = {
         test: /\.(png|woff|woff2|eot|ttf|svg|ico|cur|mp4)$/,
         loaders: ['file-loader'],
       },
+
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+
       {
         test: /\.(js|jsx)$/i,
         use: ['babel-loader'],
       },
+
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+          },
+          {
+            loader: require.resolve('react-docgen-typescript-loader'),
+          },
+        ],
+      },
     ];
+
+    config.resolve.extensions.push('.ts', '.tsx');
 
     return config;
   },
