cebf824d88e318c775d9d92dda820c12e7835bef
ggdaltoso <ggdaltoso@gmail.com>
2020-08-07 14:51:23 -0300

    docs(input): add TS to stories

---
+++ packages/core/stories/input.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+// import withClippy from '../.storybook/src/clippy-addon/clippy-addon';
+
+import Input from '../components/Input';
+
+const code = '<Input />';
+
+storiesOf('Input', module)
+  // .addDecorator(withClippy)
+  .addParameters({ code })
+  .add('default', () => <Input />);
