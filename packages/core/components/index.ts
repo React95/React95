ccb4a323502f4616dc8c8e3c2a9ffbe95aa797ce
ggdaltoso <ggdaltoso@gmail.com>
2020-08-12 09:56:03 -0300

    feat(ts): exporting new TS Components

--- packages/core/components/index.ts
+++ packages/core/components/index.ts
@@ -1,3 +1,6 @@
+import { ModalContext } from './Modal';
 import Input from './Input';
+import ThemeProvider from './ThemeProvider';
+import GlobalStyle from './GlobalStyle';
 
-export { Input };
+export { Input, ModalContext, ThemeProvider, GlobalStyle };
