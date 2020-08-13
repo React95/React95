7f63c6ab994bda113a0803f11ef49142d06adbb4
ggdaltoso <ggdaltoso@gmail.com>
2020-08-13 09:32:45 -0300

    refactor(stories): input story as CSF

---
+++ packages/core/stories/input.stories.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Input from '../components/Input';
+
+export default {
+  title: 'Input',
+  Component: Input,
+};
+
+export const Basic = () => <Input />;
