7719fc73ce2cc7d997d6c816dc9d1ec51031de1c
ggdaltoso <ggdaltoso@gmail.com>
2020-08-17 08:52:00 -0300

    docs(button): doc button as TSX file

---
+++ packages/core/stories/button.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+
+import Button from '../components/Button/Button';
+
+export default {
+  title: 'Button',
+  Component: Button,
+} as Meta;
+
+export const Simple = () => <Button>Ok</Button>;
--- packages/core/stories/input.stories.tsx
+++ packages/core/stories/input.stories.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Input, InputProps } from '../components/Input/Input';
-console.log('Input', Input);
 
 export default {
   title: 'Input',
