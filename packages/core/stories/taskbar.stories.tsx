5066806187b95bc3b1a905e931881e5bdfd36e49
ggdaltoso <ggdaltoso@gmail.com>
2020-09-02 10:17:32 -0300

    docs(taskbar): update TaskBar docs

--- packages/core/stories/taskbar.stories.tsx
+++ packages/core/stories/taskbar.stories.tsx
@@ -1,15 +1,18 @@
-/* eslint-disable react/jsx-wrap-multilines */
-import React, { useState } from 'react';
-import { storiesOf } from '@storybook/react';
-import withClippy from '../.storybook/src/clippy-addon/clippy-addon';
+import * as React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
 
 import TaskBar from '../components/TaskBar';
 import { Modal } from '../components/Modal';
 import List from '../components/List';
 
-const TaskBarStory = () => {
-  const [first, toggleFirst] = useState(false);
-  const [second, toggleSecond] = useState(false);
+export default {
+  title: 'TaskBar',
+  component: TaskBar,
+} as Meta;
+
+export const Simple = () => {
+  const [first, toggleFirst] = React.useState(false);
+  const [second, toggleSecond] = React.useState(false);
 
   const closeFirst = () => toggleFirst(false);
   const closeSecond = () => toggleSecond(false);
@@ -21,16 +24,16 @@ const TaskBarStory = () => {
           icon="windows_explorer"
           title="Windows Explorer"
           closeModal={closeFirst}
-          width={300}
-          height={200}
+          width="300"
+          height="200"
         />
       )}
 
       {second && (
         <Modal
           defaultPosition={{ x: 50, y: 50 }}
-          width={300}
-          height={200}
+          width="300"
+          height="200"
           icon="reader_closed"
           title="Local Disk (C:)"
           closeModal={closeSecond}
@@ -57,27 +60,3 @@ const TaskBarStory = () => {
     </>
   );
 };
-
-const code = `<TaskBar
-  list={
-    <List>
-      <List.Item 
-        icon="reader_closed" 
-        onClick={() => { . . . }}
-      >
-        Local Disk (C:)
-      </List.Item>
-      <List.Item
-        icon="windows_explorer"
-        onClick={() => { . . . }}
-      >
-        Windows Explorer
-      </List.Item>
-    </List>
-  }
-/>`;
-
-storiesOf('TaskBar', module)
-  .addDecorator(withClippy)
-  .addParameters({ code })
-  .add('default', () => <TaskBarStory />);
