0596b376a2592a8541651d83e7f5d0c98705b681
ggdaltoso <ggdaltoso@gmail.com>
2020-10-27 11:00:36 -0300

    refactor(icons): fix filename extension

--- packages/icons/toPNG.ts
+++ packages/icons/toPNG.ts
@@ -6,12 +6,15 @@ const DEST_FOLDER = './src/png';
 
 const files = fs.readdirSync(ICONS_FOLDER);
 
-files.forEach(icon => {
+files.slice(0,1).forEach(icon => {
   const buffer = fs.readFileSync(`${ICONS_FOLDER}/${icon}`);
 
   ICO.parse(buffer, 'image/png').then(images => {
     images.forEach(image => {
-      let fileName = `${icon}_${image.width}x${image.height}-${image.bpp}`;
+      const [iconName] = icon.split('.');
+
+      let fileName = `${iconName}_${image.width}x${image.height}-${image.bpp}`;
+
       if (fs.existsSync(`${DEST_FOLDER}/${fileName}bit.png`)) {
         fileName = `${fileName}bit_(1).png`;
       } else {
