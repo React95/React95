5710158efc398cf7443bce3ec2bd689b49f89e13
ggdaltoso <ggdaltoso@gmail.com>
2020-10-27 10:44:37 -0300

    feat(topng): create a script to generate png for each icon
    following the pattern {iconname}_{width}x{height}-{image.bpp}bit_({variant}).png

---
+++ packages/icons/toPNG.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import ICO from 'icojs';
+
+const ICONS_FOLDER = './src/icons';
+const DEST_FOLDER = './src/png';
+
+const files = fs.readdirSync(ICONS_FOLDER);
+
+files.forEach(icon => {
+  const buffer = fs.readFileSync(`${ICONS_FOLDER}/${icon}`);
+
+  ICO.parse(buffer, 'image/png').then(images => {
+    images.forEach(image => {
+      let fileName = `${icon}_${image.width}x${image.height}-${image.bpp}`;
+      if (fs.existsSync(`${DEST_FOLDER}/${fileName}bit.png`)) {
+        fileName = `${fileName}bit_(1).png`;
+      } else {
+        fileName = `${fileName}bit.png`;
+      }
+
+      const data = Buffer.from(image.buffer);
+      const fileDestination = `${DEST_FOLDER}/${fileName}`;
+
+      fs.writeFileSync(fileDestination, data);
+      console.log(`icon ${fileDestination} created`);
+    });
+  });
+});
+
+
+  
