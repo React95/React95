1b176dbfe8572d9ed68f1a6aa9918039049f7fa5
ggdaltoso <ggdaltoso@gmail.com>
2020-11-13 10:24:04 -0300

    docs(textarea): add Figma for TextArea component
    Co-authored-by: @janneilkka

--- packages/core/stories/textarea.stories.tsx
+++ packages/core/stories/textarea.stories.tsx
@@ -22,3 +22,11 @@ export const Simple = () => {
     />
   );
 };
+
+Simple.parameters = {
+  design: {
+    type: 'figma',
+    url:
+      'https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A18',
+  },
+};
