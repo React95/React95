d21f0bcd9f3fc1b1a7b05b18fb93eb68819730f4
ggdaltoso <ggdaltoso@gmail.com>
2021-06-04 20:55:26 -0300

    refactor(Dropdown): adjust to the new theme
    Co-authored-by: @belakm

--- packages/core/components/Dropdown/Dropdown.tsx
+++ packages/core/components/Dropdown/Dropdown.tsx
@@ -22,11 +22,11 @@ const Wrapper = styled.div`
     right: 1px;
     top: 2px;
 
-    background-color: bg;
-    box-shadow: inset 0.5px 0.7px 0px 0.7px ${th('colors.grays.2')},
+    background-color: material;
+    box-shadow: inset 0.5px 0.7px 0px 0.7px ${th('colors.material')},
       inset -1px 0px 0 1px ${th(
-        'colors.grays.3',
-      )}, inset 1.5px 1.5px 0px 1.5px ${th('colors.white')};
+        'colors.borderDark',
+      )}, inset 1.5px 1.5px 0px 1.5px ${th('colors.borderLightest')};
 
     border-right: 1;
     border-bottom: 1;
@@ -47,16 +47,16 @@ const Select = styled.select`
 
   padding: 3;
 
-  background-color: white;
+  background-color: canvas;
 
   border-left: 1;
-  border-left-color: grays.3;
+  border-left-color: borderDark;
   border-top: 1;
-  border-top-color: grays.3;
+  border-top-color: borderDark;
 
-  box-shadow: inset -1px -1px 0 0 ${th('colors.grays.2')},
-    inset 1px 1px 0 0 ${th('colors.black')},
-    0.5px 0.5px 0 0.5px ${th('colors.white')};
+  box-shadow: inset -1px -1px 0 0 ${th('colors.material')},
+    inset 1px 1px 0 0 ${th('colors.borderDarkest')},
+    0.5px 0.5px 0 0.5px ${th('colors.borderLightest')};
 
   -webkit-appearance: none;
   -moz-appearance: none;
