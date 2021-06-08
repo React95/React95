716d26f27102aadd6be6ef9d9f3fb2cf77f1d895
ggdaltoso <ggdaltoso@gmail.com>
2021-06-08 09:56:19 -0300

    chore(storybook): remove @storybook/addons reference
    Co-authored-by: @tchock

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -1,9 +1,6 @@
 import { addParameters } from '@storybook/react';
-import { addons } from '@storybook/addons';
 
-import theme from './theme';
 import Frame from './decorators/Frame';
-
 import { withThemes } from './src/theme-changer/withThemes';
 
 export const globals = {
@@ -19,11 +16,6 @@ addParameters({
     name: 'React95',
     url: 'https://github.com/React95/React95',
   },
-});
-
-addons.setConfig({
-  panelPosition: 'bottom',
-  theme,
 });
 
 export const decorators = [Frame, withThemes];
