1071afe74467c5f9783be999424a0bd06058bcc9
ggdaltoso <ggdaltoso@gmail.com>
2021-08-27 19:45:00 -0300

    chore(dps): update scripts and update dependencies to work with mdx content

--- packages/gatsby-theme/gatsby-config.js
+++ packages/gatsby-theme/gatsby-config.js
@@ -1,1 +1,14 @@
-module.exports = {};
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content/`,
+      },
+    },
+    'gatsby-plugin-mdx-frontmatter',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-typescript',
+  ],
+};
