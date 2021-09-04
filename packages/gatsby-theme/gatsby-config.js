05875c04c59ce55511a0eb03302ee83e4f702c5d
ggdaltoso <ggdaltoso@gmail.com>
2021-09-04 19:13:10 -0300

    chore(gatsby-theme): adjusting the gatsby-theme package
    updating publishConfig to publish the entire folder and adding configurations to run the
    gatsby-theme as a standalone package, without needing to run the explorer

--- packages/gatsby-theme/gatsby-config.js
+++ packages/gatsby-theme/gatsby-config.js
@@ -1,14 +1,19 @@
-module.exports = {
+const config = ({ contentPath = 'content', basePath = '/' } = {}) => ({
+  siteMetadata: {
+    basePath,
+  },
   plugins: [
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
-        path: `${__dirname}/content/`,
+        path: contentPath,
       },
     },
     'gatsby-plugin-mdx-frontmatter',
     'gatsby-plugin-mdx',
     'gatsby-plugin-typescript',
   ],
-};
+});
+
+module.exports = process.env.THEME_STANDALONE ? config() : config;
