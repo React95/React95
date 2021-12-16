7643d3b3c7859c8fe6c082abfcdcc943b252831a
ggdaltoso <ggdaltoso@gmail.com>
2021-12-15 21:30:50 -0300

    docs(Avatar): simplifying docs and adding square letters example

--- packages/core/stories/avatar.stories.tsx
+++ packages/core/stories/avatar.stories.tsx
@@ -2,26 +2,41 @@ import * as React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 
 import Avatar from '../components/Avatar';
+import { Frame } from '../components';
 
 export default {
   title: 'Avatar',
   component: Avatar,
 } as Meta;
 
-export const Simple = () => <Avatar src="https://github.com/React95.png" alt="photo" />;
+export const Simple = () => (
+  <Avatar src="https://github.com/React95.png" alt="photo" />
+);
 
 Simple.parameters = {
-    design: { disable: true },
-};
-  
-export const WithCircle = () => <Avatar src="https://github.com/React95.png" alt="photo" circle />
-
-WithCircle.parameters = {
-    design: { disable: true },
+  design: { disable: true },
 };
 
-export const WithLettersAvatar = () => <Avatar circle> <p>GG</p> </Avatar>
+export const Circle = () => (
+  <Avatar src="https://github.com/React95.png" alt="photo" circle />
+);
 
-WithLettersAvatar.parameters = {
-    design: { disable: true },
+Circle.parameters = {
+  design: { disable: true },
+};
+
+export const Letters = () => (
+  <Frame
+    display="inline-flex"
+    boxShadow="none"
+    bg="transparent"
+    style={{ gap: 8 }}
+  >
+    <Avatar>SQ</Avatar>
+    <Avatar circle>RO</Avatar>
+  </Frame>
+);
+
+Letters.parameters = {
+  design: { disable: true },
 };
