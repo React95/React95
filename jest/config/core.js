409d42e5f48060bf9bdb47a4ba2f55e45f687cf1
ggdaltoso <ggdaltoso@gmail.com>
2022-03-08 15:30:14 -0300

    chore(jest): update configs

--- jest/config/core.js
+++ jest/config/core.js
@@ -1,21 +1,22 @@
 module.exports = {
   testMatch: [
-    '<rootDir>/../../packages/core/**/*.test.ts',
-    '<rootDir>/../../packages/core/**/*.test.tsx',
+    '<rootDir>/packages/core/**/*.test.ts',
+    '<rootDir>/packages/core/**/*.test.tsx',
   ],
-  roots: ['<rootDir>/../../'],
+  rootDir: './../../',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
-      '<rootDir>/../mocks/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/../mocks/styleMock.js',
+      '<rootDir>/jest/mocks/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/jest/mocks/styleMock.js',
   },
   displayName: 'core',
+  name: 'core',
   setupFilesAfterEnv: [
     'babel-polyfill',
     '@testing-library/jest-dom/extend-expect',
     'jest-styled-components',
-    '<rootDir>/../setup/core.setup.js',
+    '<rootDir>/jest/setup/core.setup.js',
   ],
-  snapshotResolver: './snapshotResolver.js',
+  snapshotResolver: '<rootDir>/jest/config/snapshotResolver.js',
   testEnvironment: 'jsdom',
 };
