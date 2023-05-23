a979b3f5eda2f0a6ecf90cf664a00a70857def61
ggdaltoso <ggdaltoso@gmail.com>
2023-05-22 21:33:33 -0300

    refactor: update some TS stuff

--- packages/core/components/Cursor/Cursor.tsx
+++ packages/core/components/Cursor/Cursor.tsx
@@ -1,35 +1,35 @@
-import { css } from 'styled-components';
 import { Property } from 'csstype';
+import { css } from 'styled-components';
 
-import Auto from './cursors/Arrow.png';
-import Text from './cursors/Text.png';
-import VerticalText from './cursors/VText.png';
-import Help from './cursors/Help.png';
-import Crosshair from './cursors/Cross.png';
-import Pointer from './cursors/HandPointer.png';
-import Progress from './cursors/AppStarting.png';
-import Wait from './cursors/Wait.png';
-import Alias from './cursors/Link.png';
-import Copy from './cursors/Copy.png';
-import Move from './cursors/Move.png';
-import NoDrop from './cursors/Circle.png';
-import NotAllowed from './cursors/NO.png';
-import Grab from './cursors/Hand.png';
-import Grabbing from './cursors/Handsqueezed.png';
-import ColResize from './cursors/ColRezise.png';
-import RowResize from './cursors/RowResize.png';
-import NResize from './cursors/UpArrow.png';
-import EResize from './cursors/RightArrow.png';
-import SResize from './cursors/DownArrow.png';
-import WResize from './cursors/LeftArrow.png';
-import NsResize from './cursors/UpDownArrow.png';
-import EwResize from './cursors/LeftRight.png';
-import NeResize from './cursors/UpRightArrow.png';
-import NwResize from './cursors/UpLeftArrow.png';
-import SeResize from './cursors/DownRightArrow.png';
-import SwResize from './cursors/DownLeftArrow.png';
-import NeswResize from './cursors/AngleUpRight.png';
 import NwseResize from './cursors/AngleUpLeft.png';
+import NeswResize from './cursors/AngleUpRight.png';
+import Progress from './cursors/AppStarting.png';
+import Auto from './cursors/Arrow.png';
+import NoDrop from './cursors/Circle.png';
+import ColResize from './cursors/ColRezise.png';
+import Copy from './cursors/Copy.png';
+import Crosshair from './cursors/Cross.png';
+import SResize from './cursors/DownArrow.png';
+import SwResize from './cursors/DownLeftArrow.png';
+import SeResize from './cursors/DownRightArrow.png';
+import Grab from './cursors/Hand.png';
+import Pointer from './cursors/HandPointer.png';
+import Grabbing from './cursors/Handsqueezed.png';
+import Help from './cursors/Help.png';
+import WResize from './cursors/LeftArrow.png';
+import EwResize from './cursors/LeftRight.png';
+import Alias from './cursors/Link.png';
+import Move from './cursors/Move.png';
+import NotAllowed from './cursors/NO.png';
+import EResize from './cursors/RightArrow.png';
+import RowResize from './cursors/RowResize.png';
+import Text from './cursors/Text.png';
+import NResize from './cursors/UpArrow.png';
+import NsResize from './cursors/UpDownArrow.png';
+import NwResize from './cursors/UpLeftArrow.png';
+import NeResize from './cursors/UpRightArrow.png';
+import VerticalText from './cursors/VText.png';
+import Wait from './cursors/Wait.png';
 import ZoomIn from './cursors/ZoomIn.png';
 import ZoomOut from './cursors/ZoomOut.png';
 
@@ -76,7 +76,7 @@ const toKebabCase = (str: string) =>
 
 export type ICursor = Record<keyof typeof cursors, Property.Cursor>;
 
-const Cursor = Object.keys(cursors).reduce(
+const Cursor = (Object.keys(cursors) as (keyof ICursor)[]).reduce(
   (all, cur) =>
     Object.assign(all, {
       [cur]: css`
