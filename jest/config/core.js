04f29fdc9a325740a96e29908af0738c8c23fa0b
ggdaltoso <ggdaltoso@gmail.com>
2023-07-30 21:29:26 -0300

    chore: mocking binary files

--- jest/config/core.js
+++ jest/config/core.js
@@ -1,27 +1,17 @@
-const snapshotResolver = require('./snapshotResolver')
+const snapshotResolver = require('./snapshotResolver');
+const { resolve } = require('path');
 
 module.exports = {
-  testMatch: [
-    '<rootDir>/packages/core/**/*.test.ts',
-    '<rootDir>/packages/core/**/*.test.tsx',
-  ],
-  rootDir: './../../',
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
-      '<rootDir>/jest/mocks/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/jest/mocks/styleMock.js',
-    'styled-components': 'styled-components/dist/styled-components.browser.cjs.js',
-  },
-  displayName: 'core',
-  name: 'core',
-  setupFilesAfterEnv: [
-    'babel-polyfill',
-    '@testing-library/jest-dom/extend-expect',
-    'jest-styled-components',
-    '<rootDir>/jest/setup/core.setup.js',
-  ],
-  environment: 'jsdom',
   test: {
+    alias: [
+      {
+        find: /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$/,
+        replacement: '',
+        customResolver() {
+          return resolve(__dirname, '../mocks/fileMock.js')
+        }
+      }],
+    name: 'core',
     globals: true,
     resolveSnapshotPath: snapshotResolver.resolveSnapshotPath,
     environment: "happy-dom",
@@ -30,6 +20,10 @@ module.exports = {
       'jest-styled-components',
       '@testing-library/jest-dom',
       '../../jest/setup/core.setup.js',
+    ],
+    testMatch: [
+      '<root>/packages/core/**/*.test.ts',
+      '<root>/packages/core/**/*.test.tsx',
     ],
   },
 };
