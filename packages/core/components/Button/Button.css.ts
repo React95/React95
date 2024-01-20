1f4e4ff73132689d6eae1fd4860978c7212b5a51
ggdaltoso <ggdaltoso@gmail.com>
2024-01-20 18:34:48 -0300

    feat(core): extract styled from Button

---
+++ packages/core/components/Button/Button.css.ts
@@ -0,0 +1,42 @@
+import { style } from '@vanilla-extract/css';
+import { calc } from '@vanilla-extract/css-utils';
+
+import { contract } from '../ThemeProvider/themes/contract.css';
+
+export const styles = style({
+  backgroundColor: contract.colors.material,
+  paddingTop: contract.space[7],
+  paddingInline: contract.space[20],
+  paddingBottom: contract.space[5],
+  border: 'none',
+  color: contract.colors.materialText,
+
+  fontSize: 12,
+
+  boxShadow: `inset 1px 1px 0px 1px ${contract.colors.borderLightest},
+    inset 0 0 0 1px ${contract.colors.borderDark},
+    1px 1px 0 0px ${contract.colors.borderDarkest}`,
+
+  ':disabled': {
+    color: contract.colors.materialTextDisabled,
+  },
+  ':focus': {
+    outline: `${contract.space[1]} dotted ${contract.colors.materialText}`,
+    outlineOffset: calc.negate(contract.space[5]),
+
+    boxShadow: `inset 1px 1px 0px 1px ${contract.colors.borderLightest},
+      inset -0.5px -0.5px 0px 1px ${contract.colors.borderDark},
+      1px 1px 0 1px ${contract.colors.borderDarkest}`,
+  },
+  ':active': {
+    paddingTop: contract.space[8],
+    paddingInline: contract.space[20],
+    paddingBottom: contract.space[4],
+
+    outline: `${contract.space[1]} dotted ${contract.colors.borderDarkest}`,
+    outlineOffset: calc.negate(contract.space[5]),
+
+    boxShadow: `inset 0 0 0 1px ${contract.colors.borderDark},
+      0 0 0 1px ${contract.colors.borderDarkest}`,
+  },
+});
--- packages/core/components/Button/Button.tsx
+++ packages/core/components/Button/Button.tsx
@@ -1,43 +1,10 @@
-import styled, { th } from '@xstyled/styled-components';
+import React, { FC } from 'react';
 
-const Button = styled.button`
-  background-color: material;
-  padding: 7 20 5;
-  border: none;
-  color: materialText;
+import { styles } from './Button.css';
 
-  font-size: 12px;
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-  box-shadow: inset 1px 1px 0px 1px ${th('colors.borderLightest')},
-    inset 0 0 0 1px ${th('colors.borderDark')},
-    1px 1px 0 0px ${th('colors.borderDarkest')};
-
-  &:disabled {
-    color: materialTextDisabled;
-  }
-
-  &:focus {
-    outline: ${th('space.1')}px dotted ${th('colors.materialText')};
-    outline-offset: -${th('space.5')}px;
-
-    box-shadow: inset 1px 1px 0px 1px ${th('colors.borderLightest')},
-      inset -0.5px -0.5px 0px 1px ${th('colors.borderDark')},
-      1px 1px 0 1px ${th('colors.borderDarkest')};
-  }
-
-  &:active {
-    padding: 8 20 4;
-
-    outline: ${th('space.1')}px dotted ${th('colors.borderDarkest')};
-    outline-offset: -${th('space.5')}px;
-
-    box-shadow: inset 0 0 0 1px ${th('colors.borderDark')},
-      0 0 0 1px ${th('colors.borderDarkest')};
-  }
-`;
-
-Button.defaultProps = {
-  children: 'Ok',
+export const Button: FC<ButtonProps> = props => {
+  return <button {...props} className={[styles, props.className].join(' ')} />;
 };
-
-export default Button;
