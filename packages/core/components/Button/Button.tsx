c590910602ccb8cb26b59f9971ed2ab64c880214
ggdaltoso <ggdaltoso@gmail.com>
2024-02-08 21:52:14 -0300

    refactor(Button): add classNames

--- packages/core/components/Button/Button.tsx
+++ packages/core/components/Button/Button.tsx
@@ -1,18 +1,22 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 
 import { button } from './Button.css';
 import Frame from '../Frame';
 import { FrameProps } from '../Frame/Frame';
+import cn from 'classnames';
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   FrameProps;
 
-export const Button: FC<ButtonProps> = props => {
-  return (
-    <Frame
-      as="button"
-      {...props}
-      className={[button, props.className].join(' ')}
-    />
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    return (
+      <Frame
+        as="button"
+        {...props}
+        className={cn(button, props.className)}
+        ref={ref}
+      />
+    );
+  },
+);
