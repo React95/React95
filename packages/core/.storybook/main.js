6b60082eb4057d7f26d1d3da0aa4b1b84bcd5994
ggdaltoso <ggdaltoso@gmail.com>
2024-02-08 22:17:23 -0300

    feat(core): Tooltip revisited!

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -6,6 +6,7 @@ import { ImageLoader } from 'esbuild-vanilla-image-loader';
 export default {
   // stories: [, '../stories/(?!all)*.stories.tsx'],
   stories: [
+    '../stories/tooltip.stories.tsx',
     '../stories/radiobutton.stories.tsx',
     '../stories/titlebar.stories.tsx',
     '../stories/range.stories.tsx',
--- packages/core/components/Tooltip/Tooltip.tsx
+++ packages/core/components/Tooltip/Tooltip.tsx
@@ -1,38 +1,14 @@
 import * as React from 'react';
-import styled from '@xstyled/styled-components';
-
-import Frame from '../Frame';
-
-const Tip = styled(Frame)`
-  background: radial-gradient(#ff0 20%, transparent 20%) 0 0,
-    radial-gradient(#ff0 20%, transparent 20%) 4px 4px,
-    radial-gradient(rgba(255, 255, 255, 0.1) 20%, transparent 25%) 0 1px,
-    radial-gradient(rgba(255, 255, 255, 0.1) 20%, transparent 25%) 3px 4px;
-  background-size: 7px 7px;
-  background-color: borderLightest;
-  border: 1;
-  padding: 2 2 2 4;
-  box-shadow: none;
-  position: absolute;
-  top: -20px;
-  text-align: center;
-  z-index: taskbar;
-`;
+import { tip, wrapper } from './Tooltip.css';
+import cn from 'classnames';
 
 export type TooltipProps = {
   text?: string;
   delay?: number;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const Wrapper = styled.div`
-  display: inline-block;
-  position: relative;
-  cursor: default;
-  white-space: nowrap;
-`;
-
 const TooltipRenderer = (
-  { children, text, delay, ...rest }: TooltipProps,
+  { children, text = 'Hello!', delay = 1000, ...rest }: TooltipProps,
   ref: React.Ref<HTMLDivElement>,
 ) => {
   const [show, setShow] = React.useState(false);
@@ -52,46 +28,19 @@ const TooltipRenderer = (
   };
 
   return (
-    <Wrapper
+    <div
+      {...rest}
+      className={cn(wrapper, rest.className)}
       onMouseEnter={handleEnter}
       onMouseLeave={handleLeave}
-      {...rest}
       ref={ref}
     >
-      {show && <Tip>{text}</Tip>}
+      {show && <div className={tip}>{text}</div>}
       {children}
-    </Wrapper>
+    </div>
   );
 };
 
-const Tooltip = React.forwardRef<HTMLDivElement, TooltipProps>(TooltipRenderer);
-
-function formatDate(date: Date): string {
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  const day = date.getDate();
-  const monthIndex = date.getMonth();
-  const year = date.getFullYear();
-
-  return `${day.toString().padStart(2, '0')} ${monthNames[monthIndex]} ${year}`;
-}
-
-Tooltip.defaultProps = {
-  delay: 1000,
-  text: formatDate(new Date()),
-};
-
-export default Tooltip;
+export const Tooltip = React.forwardRef<HTMLDivElement, TooltipProps>(
+  TooltipRenderer,
+);
