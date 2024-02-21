a12da9fd0d30acb041b8ad57cffcd7f14ba1b558
ggdaltoso <ggdaltoso@gmail.com>
2024-02-20 22:02:32 -0300

    refactor(Range): extending `className`

--- packages/core/components/Range/Range.tsx
+++ packages/core/components/Range/Range.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { Frame, FrameProps } from '../Frame/Frame';
 import { range } from './Range.css';
+import cn from 'classnames';
 
 export type RangeProps = Omit<FrameProps<'input'>, 'as'>;
 
 export const Range = React.forwardRef<HTMLInputElement, RangeProps>(
   (rest, ref) => (
-    <Frame {...rest} ref={ref} className={range} as="input" type="range" />
+    <Frame
+      {...rest}
+      ref={ref}
+      className={cn(range, rest.className)}
+      as="input"
+      type="range"
+    />
   ),
 );
