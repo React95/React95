bda3479354fa5db9a2130bdd35e17f77b90edf15
ggdaltoso <ggdaltoso@gmail.com>
2024-02-27 22:19:24 -0300

    refactor: extracting themes by hand

--- packages/core/.storybook/decorators/Frame.jsx
+++ packages/core/.storybook/decorators/Frame.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../components/GlobalStyle/GlobalStyle.css';
-import { win95 } from '../../components/ThemeProvider/themes/win95.css';
+import * as themes from '../../components/ThemeProvider/theme';
 
 const Frame = Story => (
-  <div style={{ padding: 10 }} className={win95}>
+  <div style={{ padding: 10 }} className={themes.win95}>
     <Story />
   </div>
 );
--- packages/core/.storybook/src/theme-changer/src/Panel.js
+++ packages/core/.storybook/src/theme-changer/src/Panel.js
@@ -1,41 +1,109 @@
 import React from 'react';
+import { useStorybookState } from '@storybook/manager-api';
 
-// this wont work
-import * as themes from '../../../../components/ThemeProvider/theme';
-
-// this wont eighter
-import { win95 } from '../../../../components/ThemeProvider/themes/win95.css';
-
-// import { Button, Frame } from '../../../../components';
-
-console.log({ themes });
-
-// const ThemeWindow = ({ name, changeTheme }) => (
-//   <Frame width="auto" m="$10">
-//     <Frame p="$4" m="$2" bg="$headerBackground" color="$headerText">
-//       {name}
-//     </Frame>
-//     <Frame p="$10">
-//       <Button
-//         onClick={() => {
-//           changeTheme({ selectedTheme: name });
-//         }}
-//       >
-//         {name}
-//       </Button>
-//     </Frame>
-//   </Frame>
-// );
-
-export const ThemePanel = ({ api }) => (
-  <div style={{ padding: '12px' }}>
-    <h1 style={{ marginBottom: '12px' }}>Pick a theme below</h1>
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {/* {Object.keys(themes).map(name => (
-        <div className={name} key={name}>
-          <ThemeWindow name={name} changeTheme={api.updateGlobals} />
-        </div>
-      ))} */}
+const ThemeWindow = ({ name, changeTheme }) => (
+  <div
+    style={{
+      width: '140px',
+      padding: 1,
+      margin: 10,
+      backgroundColor: 'var(--r95_g5jstu11)',
+      boxShadow:
+        'inset 1px 1px 0px 1px var(--r95_g5jstuu), inset 0 0 0 1px var(--r95_g5jstuq), 1px 1px 0 0px var(--r95_g5jstur)',
+    }}
+  >
+    <div
+      style={{
+        background: 'var(--r95_g5jstux)',
+        padding: 4,
+        margin: 2,
+        color: 'var(--r95_g5jstu10)',
+      }}
+    >
+      {name}
+    </div>
+    <div style={{ padding: 10 }}>
+      <button
+        style={{
+          backgroundColor: 'var(--r95_g5jstu11)',
+          paddingTop: 'var(--r95_g5jstua)',
+          paddingInline: 'var(--r95_g5jstun)',
+          paddingBottom: 'var(--r95_g5jstu8)',
+          border: 'none',
+          color: 'var(--r95_g5jstu12)',
+          fontSize: '12px',
+          boxShadow:
+            'inset 1px 1px 0px 1px var(--r95_g5jstuu), inset 0 0 0 1px var(--r95_g5jstuq), 1px 1px 0 0px var(--r95_g5jstur)',
+        }}
+        onClick={() => {
+          changeTheme({ selectedTheme: name });
+        }}
+      >
+        {name}
+      </button>
     </div>
   </div>
 );
+
+export const ThemePanel = ({ api }) => {
+  const state = useStorybookState();
+  const [themes, setThemes] = React.useState();
+
+  React.useEffect(() => {
+    if (!state.previewInitialized) {
+      return;
+    }
+
+    const [previewFrame] = document.getElementsByTagName('iframe');
+
+    const extractedThemes = {};
+
+    for (let link of previewFrame.contentDocument.querySelectorAll(
+      'link, style',
+    )) {
+      try {
+        if (!link.sheet) {
+          console.warn(
+            'Warning:',
+            "Property 'sheet' is not set on element",
+            link,
+          );
+        } else if (link.dataset.file.includes('ThemeProvider')) {
+          const fileName = link.dataset.file.split('/').at(-1);
+          const [themeName] = fileName.split('.');
+
+          const [themeClassName] = link.sheet.rules;
+
+          extractedThemes[themeName] = themeClassName.selectorText.substring(1);
+
+          const newLink = document.createElement('style');
+          newLink.innerHTML = themeClassName.cssText;
+
+          document.head.appendChild(newLink);
+        }
+      } catch (e) {
+        console.warn(
+          'Warning:',
+          e.message,
+          ". Try set crossorigin='anonymous' on element",
+          link,
+        );
+      }
+    }
+
+    setThemes(extractedThemes);
+  }, [state.previewInitialized]);
+
+  return themes ? (
+    <div style={{ padding: '12px' }}>
+      <h1 style={{ marginBottom: '12px' }}>Pick a theme below</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {Object.entries(themes).map(([name, className]) => (
+          <div className={className} key={name}>
+            <ThemeWindow name={name} changeTheme={api.updateGlobals} />
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : null;
+};
