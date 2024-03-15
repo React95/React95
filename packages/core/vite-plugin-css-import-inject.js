ac4145fbdaa10cae7c7991e962f0b2be58caad0c
ggdaltoso <ggdaltoso@gmail.com>
2024-03-14 21:57:02 -0300

    refactor: injecting generated CSS in the final `vanilla-extract` file

--- packages/core/vite-plugin-css-import-inject.js
+++ packages/core/vite-plugin-css-import-inject.js
@@ -23,7 +23,7 @@ export const cssImportInject = () => {
       for (const file of files) {
         // skip entry file (index.js)
         // and generated css files (*.css.ts.vanilla.css)
-        if (bundle[file].isEntry || file.endsWith('css')) {
+        if (bundle[file].isEntry || !file.endsWith('css.js')) {
           continue;
         }
 
@@ -39,11 +39,14 @@ export const cssImportInject = () => {
           cssLookup = component;
         }
 
+        const cssVanillaExtractFile = files.find(f =>
+          f.endsWith(`${cssLookup}.css.js`),
+        );
         const cssFile = files.find(f =>
           f.endsWith(`${cssLookup}.css.ts.vanilla.css`),
         );
 
-        if (cssFile) {
+        if (cssFile && cssVanillaExtractFile) {
           const cssBundleFile = bundle[cssFile];
 
           const filePath = path.resolve('dist', file);
