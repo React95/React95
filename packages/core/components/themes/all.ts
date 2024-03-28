1efd73c4520a648f304da01ecd01504e13f25ccb
ggdaltoso <ggdaltoso@gmail.com>
2024-03-28 17:16:50 -0300

    refactor: sorting all themes

--- packages/core/components/themes/all.ts
+++ packages/core/components/themes/all.ts
@@ -1,73 +1,73 @@
-import * as win95 from './win95.css';
-import * as counterStrike from './counterStrike.css';
-import * as bee from './bee.css';
-import * as pamelaAnderson from './pamelaAnderson.css';
 import * as azureOrange from './azureOrange.css';
-import * as olive from './olive.css';
-import * as vaporTeal from './vaporTeal.css';
-import * as matrix from './matrix.css';
-import * as vermillion from './vermillion.css';
-import * as tooSexy from './tooSexy.css';
-import * as ninjaTurtles from './ninjaTurtles.css';
-import * as tokyoDark from './tokyoDark.css';
-import * as molecule from './molecule.css';
-import * as travel from './travel.css';
-import * as theSixtiesUSA from './theSixtiesUSA.css';
-import * as candy from './candy.css';
-import * as modernDark from './modernDark.css';
-import * as storm from './storm.css';
-import * as millenium from './millenium.css';
-import * as spruce from './spruce.css';
-import * as slate from './slate.css';
-import * as rose from './rose.css';
-import * as rainyDay from './rainyDay.css';
-import * as plum from './plum.css';
-import * as marine from './marine.css';
-import * as maple from './maple.css';
-import * as lilac from './lilac.css';
+import * as bee from './bee.css';
 import * as blackAndWhite from './blackAndWhite.css';
-import * as highContrast from './highContrast.css';
-import * as eggplant from './eggplant.css';
 import * as brick from './brick.css';
-import * as water from './water.css';
+import * as candy from './candy.css';
 import * as coldGray from './coldGray.css';
+import * as counterStrike from './counterStrike.css';
+import * as eggplant from './eggplant.css';
+import * as highContrast from './highContrast.css';
+import * as lilac from './lilac.css';
 import * as lilacRoseDark from './lilacRoseDark.css';
+import * as maple from './maple.css';
+import * as marine from './marine.css';
+import * as matrix from './matrix.css';
+import * as millenium from './millenium.css';
+import * as modernDark from './modernDark.css';
+import * as molecule from './molecule.css';
+import * as ninjaTurtles from './ninjaTurtles.css';
+import * as olive from './olive.css';
+import * as pamelaAnderson from './pamelaAnderson.css';
+import * as plum from './plum.css';
+import * as rainyDay from './rainyDay.css';
+import * as rose from './rose.css';
+import * as slate from './slate.css';
+import * as spruce from './spruce.css';
+import * as storm from './storm.css';
+import * as theSixtiesUSA from './theSixtiesUSA.css';
+import * as tokyoDark from './tokyoDark.css';
+import * as tooSexy from './tooSexy.css';
+import * as travel from './travel.css';
+import * as vaporTeal from './vaporTeal.css';
+import * as vermillion from './vermillion.css';
 import * as violetDark from './violetDark.css';
+import * as water from './water.css';
+import * as win95 from './win95.css';
 
 export {
-  win95,
-  counterStrike,
-  bee,
-  pamelaAnderson,
   azureOrange,
-  olive,
-  vaporTeal,
-  matrix,
-  vermillion,
-  tooSexy,
-  ninjaTurtles,
-  tokyoDark,
-  molecule,
-  travel,
-  theSixtiesUSA,
-  candy,
-  modernDark,
-  storm,
-  millenium,
-  spruce,
-  slate,
-  rose,
-  rainyDay,
-  plum,
-  marine,
-  maple,
-  lilac,
+  bee,
   blackAndWhite,
-  highContrast,
-  eggplant,
   brick,
-  water,
+  candy,
   coldGray,
+  counterStrike,
+  eggplant,
+  highContrast,
+  lilac,
   lilacRoseDark,
+  maple,
+  marine,
+  matrix,
+  millenium,
+  modernDark,
+  molecule,
+  ninjaTurtles,
+  olive,
+  pamelaAnderson,
+  plum,
+  rainyDay,
+  rose,
+  slate,
+  spruce,
+  storm,
+  theSixtiesUSA,
+  tokyoDark,
+  tooSexy,
+  travel,
+  vaporTeal,
+  vermillion,
   violetDark,
+  water,
+  win95,
 };
