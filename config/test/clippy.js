e1541a296dca1ecfbdb2c7d8e036da980733aa27
ggdaltoso <ggdaltoso@gmail.com>
2024-08-26 16:48:13 -0300

    test(clippy): updating tests to reflect new changes

--- config/test/clippy.js
+++ config/test/clippy.js
@@ -1,13 +1,21 @@
+import snapshotResolver from './snapshotResolver';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
 export default {
   test: {
     testMatch: [
-      '<rootDir>/packages/clippy/**/*.test.js',
-      '<rootDir>/packages/clippy/**/*.test.jsx',
+      '<rootDir>/packages/clippy/**/*.test.ts',
+      '<rootDir>/packages/clippy/**/*.test.tsx',
     ],
     name: 'clippy',
+    globals: true,
+    resolveSnapshotPath: snapshotResolver.resolveSnapshotPath,
     setupFiles: [
+      'babel-polyfill',
+      '@testing-library/jest-dom',
       '../../config/setup/clippy.setup.js',
     ],
-    environment: "happy-dom",
-  }
+    environment: 'happy-dom',
+    plugins: [vanillaExtractPlugin()],
+  },
 };
