2184817bb5371190260a39bbb725f5ae0705cf35
hawwokitty <hannesofiea@gmail.com>
2024-10-11 14:49:57 +0200

    docs(minimize docs): added minimize modal as a story under Modal

--- packages/core/stories/taskbar.stories.tsx
+++ packages/core/stories/taskbar.stories.tsx
@@ -26,7 +26,6 @@ export const Simple = {
             icon={<WindowsExplorer variant="16x16_4" />}
             title="Windows Explorer"
             titleBarOptions={[
-              <Modal.Minimize key="minimize" />,
               <TitleBar.Close key="close" onClick={closeFirst} />,
             ]}
             width="300px"
@@ -44,7 +43,6 @@ export const Simple = {
             icon={<ReaderClosed variant="16x16_4" />}
             title="Local Disk (C:)"
             titleBarOptions={[
-              <Modal.Minimize key="minimize" />,
               <TitleBar.Close key="close" onClick={closeSecond} />,
             ]}
           />
