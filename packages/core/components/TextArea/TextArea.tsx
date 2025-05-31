392be67c10f56d73c428efa862a4ca03c388d9a5
ggdaltoso <ggdaltoso@gmail.com>
2025-05-31 10:42:49 -0300

    fix: add className support to TextArea component
    This change allows the TextArea component to accept a className prop, enabling users to apply custom styles alongside the default textarea styles.

--- packages/core/components/TextArea/TextArea.tsx
+++ packages/core/components/TextArea/TextArea.tsx
@@ -1,12 +1,16 @@
 import React, { forwardRef } from 'react';
+import cn from 'classnames';
 
 import { Frame, FrameProps } from '../Frame/Frame';
 import { input as textarea } from '../Input/Input.css';
 
 export type TextAreaProps = Omit<FrameProps<'textarea'>, 'as'>;
 
-export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  (rest, ref) => (
-    <Frame {...rest} ref={ref} className={textarea} as="textarea" />
-  ),
-);
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>((rest, ref) => (
+  <Frame
+    {...rest}
+    ref={ref}
+    className={cn(textarea, rest.className)}
+    as="textarea"
+  />
+));
