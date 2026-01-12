d80012d33eaf679f1d7d442288900c20aabb01de
ggdaltoso <ggdaltoso@users.noreply.github.com>
2026-01-12 18:57:39 +0000

    deploy: 26a4a5338a87329b19715b434dc2009f99eae8ec

---
+++ assets/dropdown.stories-79ac77e5.js
@@ -0,0 +1,9 @@
+import{r as n}from"./index-9fa1aa67.js";import{D as i}from"./Dropdown-e0248f77.js";import"./index-923230fa.js";/* empty css                                */import"./Frame-f7ea70b0.js";const d={title:"Dropdown",component:i,tags:["autodocs"]},e={render:()=>n.createElement(i,null),parameters:{design:{type:"figma",url:"https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A6"}}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <Dropdown />,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A6'
+    }
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const g=["Simple"];export{e as Simple,g as __namedExportsOrder,d as default};
