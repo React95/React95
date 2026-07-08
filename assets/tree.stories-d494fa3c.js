cb4567460659cdea05221bdb401c2e5ea44846cb
ggdaltoso <ggdaltoso@users.noreply.github.com>
2026-07-08 21:29:46 +0000

    deploy: bdd1d44e50b1c674f144f7bd862686ebdcbee1b2

--- assets/tree.stories-d494fa3c.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-e5910377.js";import{T as t}from"./Tree-3a8e97c5.js";import{E as s}from"./Write1-229e0d0c.js";import"./index-66a3a5c2.js";import"./index-626ba1ae.js";import"./createRuntimeFn-62c9670f.esm-edc6fb2c.js";import"./Frame-ad175eaa.js";const{icons:i}=t,l={data:[{id:0,label:"Applications",children:[{id:1,label:"virus.exe",icon:e.jsx(i.FILE_EXECUTABLE,{variant:"16x16_4"})}]},{id:2,label:"Music",children:[{id:3,label:"Indie",children:[{id:4,label:"Weezer",icon:e.jsx(i.FILE_MEDIA,{variant:"16x16_4"}),onClick:()=>alert("nice!")},{id:5,label:"Supergrass",icon:e.jsx(i.FILE_MEDIA,{variant:"16x16_4"})}]}]},{id:3,label:"Other",children:[{id:0,label:"Fira Code.ttf",icon:e.jsx(i.FILE_FONT,{variant:"16x16_4"})},{id:1,label:"Journal.txt",icon:e.jsx(i.FILE_TEXT,{variant:"16x16_4"})}]},{id:4,label:"config.cfg",icon:e.jsx(i.FILE_SETTINGS,{variant:"16x16_4"})},{id:5,label:"random_file",icon:e.jsx(i.FILE_UNKNOWN,{variant:"16x16_4"})}]},c={id:6,label:"My Computer",icon:e.jsx(s,{variant:"16x16_4"})},u={title:"Tree",component:t,tags:["autodocs"]},r={render:()=>e.jsx(t,{...l,root:c}),parameters:{design:{type:"figma",url:"https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A20"}}};var a,o,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: () => <Tree {...treeNodes} root={root} />,
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A20'
-    }
-  }
-}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const b=["Simple"];export{r as Simple,b as __namedExportsOrder,u as default};
