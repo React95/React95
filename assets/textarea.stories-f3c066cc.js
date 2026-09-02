0a5f7dcccc7a369d372d2488c53523bece7cc852
ggdaltoso <ggdaltoso@users.noreply.github.com>
2026-09-02 20:02:27 +0000

    deploy: f2f6ee1dfeef906741edb83573e7e37cd5383284

---
+++ assets/textarea.stories-f3c066cc.js
@@ -0,0 +1,9 @@
+import{j as s}from"./jsx-runtime-e5910377.js";import{r as p}from"./index-66a3a5c2.js";import{T as a}from"./TextArea-4ef0a52d.js";import"./index-626ba1ae.js";import"./Frame-ad175eaa.js";import"./Input.css-f9ede444.js";/* empty css                              */const j={title:"TextArea",component:a,tags:["autodocs"]},c=()=>{const[i,m]=p.useState("");return s.jsx(a,{rows:10,cols:50,value:i,onChange:({target:{value:n}})=>m(n)})},e={render:()=>s.jsx(c,{}),parameters:{design:{type:"figma",url:"https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A18"}}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <SimpleDemo />,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/2cbigNitjcruBDZT12ixIq/React95-Design-Kit?node-id=3%3A18'
+    }
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const D=["Simple"];export{e as Simple,D as __namedExportsOrder,j as default};
